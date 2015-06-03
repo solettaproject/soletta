e837b566a3ddd4b81f7602a14458d9fa8a3667e3
Flavio Ceolin <flavio.ceolin@intel.com>
2015-06-03 18:20:50 -0300

    flow:process: stdout/stderr implementation
    These two nodes are part of the process node. They have an input
    port (blob) where they receive the that will be written. As they
    use nonblocking file descriptors all data comes is put in queue
    and used as soon the file descriptor is able to be written.
    
    There is a 'CLOSED' port to report when stdout/stderr is closed, there
    is an initial packet to say what's the current status at the time of the
    connection.
    
    The only difference between the nodes is the file descriptor they work.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- src/modules/flow/process/extra_rules.mk
+++ src/modules/flow/process/extra_rules.mk
@@ -1,7 +1,9 @@
 flowmodules_process_extra_src += \
 	src/modules/flow/process/common.h \
-	src/modules/flow/process/stdin.c
+	src/modules/flow/process/stdin.c \
+	src/modules/flow/process/output.c
 
 EXTRA_DIST += \
 	src/modules/flow/process/stdin.c \
+	src/modules/flow/process/output.c \
 	src/modules/flow/process/process.json
