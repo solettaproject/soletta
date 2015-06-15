7e1c0e3b0661958bd25a22bdcec06fbb0918f533
Murilo Belluzzo <murilo.belluzzo@intel.com>
2015-06-15 17:30:37 -0300

    sol-pin-mux: Setup mux during initialization
    Try to identify the platform and setup its multiplexer if exists
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- src/lib/common/sol-pin-mux.c
+++ src/lib/common/sol-pin-mux.c
@@ -39,14 +39,12 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 
-#define SOL_LOG_DOMAIN &_log_domain
 #include "sol-log-internal.h"
-SOL_LOG_INTERNAL_DECLARE_STATIC(_log_domain, "pin-mux");
-
 #include "sol-util.h"
 #include "sol-pin-mux.h"
 #include "sol-pin-mux-impl.h"
 #include "sol-pin-mux-builtins-gen.h"
+#include "sol-platform.h"
 
 #define GPIO_DRIVE_PULLUP   0
 #define GPIO_DRIVE_PULLDOWN 1
@@ -55,13 +53,12 @@ SOL_LOG_INTERNAL_DECLARE_STATIC(_log_domain, "pin-mux");
 
 #define BASE "/sys/class/gpio"
 
+SOL_LOG_INTERNAL_DECLARE(_sol_pin_mux_log_domain, "pin-mux");
+
 static const struct sol_pin_mux *mux = NULL;
 
-static void
-_log_init(void)
-{
-    SOL_LOG_INTERNAL_INIT_ONCE;
-}
+int sol_pin_mux_init(void);
+void sol_pin_mux_shutdown(void);
 
 #ifdef ENABLE_DYNAMIC_MODULES
 static void *dl_handle = NULL;
@@ -82,8 +79,8 @@ _load_mux(const char *name)
 
     handle = dlopen(path, RTLD_LAZY | RTLD_LOCAL | RTLD_NODELETE);
     if (!handle) {
-        SOL_WRN("Could not load platform pin multiplexer '%s': %s", path, dlerror());
-        return false;
+        SOL_INF("Could not load platform pin multiplexer '%s': %s", path, dlerror());
+        return true; // Not find a mux isn't necessarily an error, so we are returning true here
     }
 
     p_sym = dlsym(handle, "SOL_PIN_MUX");
@@ -135,15 +132,36 @@ _find_mux(const char *name)
 SOL_API bool
 sol_pin_mux_select_mux(const char *_board)
 {
-    _log_init();
-
     if (!_board || _board[0] == '\0')
-        return false;
+        return true;
 
     if (mux && streq(_board, mux->plat_name))
         return true;
 
+    // it only returns error if found a mux but failed to setup it.
     return _find_mux(_board) ? true : _load_mux(_board);
+}
+
+int
+sol_pin_mux_init(void)
+{
+    sol_log_domain_init_level(SOL_LOG_DOMAIN);
+
+    if (!sol_pin_mux_select_mux(sol_platform_get_name()))
+        SOL_WRN("Pin Multiplexer found, but failed to be loaded.");
+
+    return 0;
+}
+
+void
+sol_pin_mux_shutdown(void)
+{
+    mux = NULL;
+#ifdef ENABLE_DYNAMIC_MODULES
+    if (dl_handle)
+        dlclose(dl_handle);
+    dl_handle = NULL;
+#endif
 }
 
 static int
