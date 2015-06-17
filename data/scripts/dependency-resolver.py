ce838682fd66b79c104ca113678e8f2c0c9e907a
Leandro Dorileo <leandro.maciel.dorileo@intel.com>
2015-06-17 15:27:15 -0300

    kconfig
    This patch introduces the kconfig as soletta build system configurator.
    
    The work flow is pretty similar to linux kernel's one, make menuconfig,
    make oldconfig, make allyesconfig, make allmodconfig and so on.
    
    The object and modules configuration are very similar too. The main files
    to look at while adding new modules and configuration options are the
    Kconfig and Makefile's spread across the source tree, where in the
    Kconfig files one will add the module vs. objects configuration i.e:
    
    obj-$(CONFIG_MY_OPTION) += my-module.mod
    obj-my-module-$(CONFIG_MY_OPTION) += my-obj.o my-obj2.o
    
    Pay attention to the my-module.mod "aberration", this .mod suffix is
    internally used to indicate this is a "soletta object", I have used
    this notation to avoid confusion between shared and builtin modules,
    while configuring the module may be either shared or static, so
    it's simply a "soletta object" beforehand.
    
    The second file class(Kconfig) is pretty the same as in the linux kernel
    source code. The main difference is the "object types", it may be one of:
    bin-*-y, obj-*-[y,m], test-*-y. The first one is used to build binaries
    like src/bin/, the second one defines a module and the third are reserved
    for unit tests.
    
    We also support external(out of tree) modules, and the way to build it
    is exactly the same as in the linux kernel, write you Makefile(in the
    external dir) and do the following:
    
    make -C $PATH_TO_SOLETTA_SRC M=$PWD
    
    To install soletta we have a target "install" and an special variable
    DESTDIR, this variable works the same way as in the autotools world.
    Having said that, a typical installation invocation would look like:
    
    make install DESTDIR=~/local-install/
    
    If DESTDIR's not provided the build system will try to install in the
    sysroot.
    
    The script autoconf.py is the autotools's autoconf replacement, it finds
    the libs dependencies and properly  set the HAVE_* variables and the CFLAGS.
    These variables are defined in two generated files Kconfig.gen and
    Makefile.gen.
    
    Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

---
+++ data/scripts/dependency-resolver.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+
+# This file is part of the Soletta Project
+#
+# Copyright (C) 2015 Intel Corporation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#   * Neither the name of Intel Corporation nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import argparse
+import os
+import subprocess
+import sys
+import tempfile
+from shutil import which
+
+pkg_config_deps = {
+    "CHECK": "check",
+    "GLIB": "glib-2.0",
+    "GTK": "gtk+-3.0",
+    "SYSTEMD": "libsystemd",
+    "UDEV": "libudev",
+}
+
+header_deps = {
+    "KDBUS": "<systemd/sd-bus.h>",
+    "PTHREAD": "<pthread.h>",
+    "RIOTOS": "<riotos/cpu.h>",
+    "DLFCN_H": "<dlfcn.h>",
+    "INTTYPES_H": "<inttypes.h>",
+    "LINUX_NETLINK_H": "<linux/netlink.h>",
+    "LINUX_RTNETLINK_H": "<linux/rtnetlink.h>",
+    "LINUX_WATCHDOG_H": "<linux/watchdog.h>",
+    "MEMORY_H": "<memory.h>",
+    "PTHREAD_H": "<pthread.h>",
+    "STDINT_H": "<stdint.h>",
+    "STDLIB_H": "<stdlib.h>",
+    "STRINGS_H": "<strings.h>",
+    "STRING_H": "<string.h>",
+    "SYS_STAT_H": "<sys/stat.h>",
+    "SYS_TYPES_H": "<sys/types.h>",
+    "UNISTD_H": "<unistd.h>",
+}
+
+exec_deps = {
+    "VALGRIND": "valgrind",
+    "LCOV": "lcov",
+    "GENHTML": "genhtml",
+}
+
+cfragment_deps = {
+    "ACCEPT4": {"fragment": "accept4(0, 0, 0, 0);", "headers": ["<sys/socket.h>"]},
+    "ISATTY": {"fragment": "isatty(0);", "headers": ["<unistd.h>"]},
+    "PPOLL": {"fragment": "ppoll(0, 0, 0, 0);", "headers": ["<poll.h>"]},
+    "DECL_STRNDUPA": {"fragment": "strndupa(0, 0);", "headers": ["<string.h>"]},
+    "DECL_IFLA_INET6_ADDR_GEN_MODE": {"fragment": "int v = IFLA_INET6_ADDR_GEN_MODE;", \
+                                      "headers": ["<netinet/in.h>", "<netinet/ether.h>", \
+                                                  "<linux/rtnetlink.h>", "<net/if.h>" \
+                                                  "<linux/if_link.h>", "<linux/if_addr.h>"]},
+}
+
+python_pkg_deps = {
+    "PYTHON_JSONSCHEMA" : "jsonschema"
+}
+
+makefile_gen = []
+kconfig_gen = []
+
+compiler = "gcc"
+cflags = ""
+
+def kconfig_add(key, enabled):
+    item = "config {config}\n{indent}bool\n{indent}default {enabled}". \
+           format(config="HAVE_%s" % key, indent="       ", enabled=enabled)
+    kconfig_gen.append(item)
+
+def pkg_config_discover():
+    for k,v in pkg_config_deps.items():
+        cmd = "pkg-config --libs %s --cflags" % v
+        enabled = "n"
+        flags = ""
+        try:
+            flags = subprocess.check_output(cmd, stderr=subprocess.STDOUT,
+                                            shell=True, universal_newlines=True)
+            flags = flags.replace("\n", "")
+            enabled = "y"
+        except subprocess.CalledProcessError as e:
+            flags = None
+
+        if flags is not None:
+            line = "ifeq (y,$(USE_%s))\n" % k
+            line = "%s%s_CFLAGS += %s\n" % (line, k, flags)
+            line = "%sendif\n" % line
+            makefile_gen.append(line)
+
+        kconfig_add(k, enabled)
+
+def python_dep_test():
+    for k,v in python_pkg_deps.items():
+        found = "y"
+        source = "import jsonschema"
+        f = tempfile.NamedTemporaryFile(suffix=".py",delete=False)
+        f.write(bytes(source, 'UTF-8'))
+        f.close()
+
+        try:
+            cmd = "%s %s" % (sys.executable, f.name)
+            subprocess.check_output(cmd, stderr=subprocess.STDOUT,
+                                    shell=True, universal_newlines=True)
+        except subprocess.CalledProcessError as e:
+            found = "n"
+
+        line = "HAVE_%s := %s\n" % (k, found)
+        makefile_gen.append(line)
+        os.unlink(f.name)
+
+def fragment_dep_test():
+    for k,v in cfragment_deps.items():
+        enabled = "y"
+
+        headers = ""
+        for h in v["headers"]:
+            headers = "%s#include %s\n" %(headers, h)
+
+        source = "%s\nint main(int argc, char **argv){\n %s return 0;\n}" % (headers, v["fragment"])
+        f = tempfile.NamedTemporaryFile(suffix=".c",delete=False)
+        f.write(bytes(source, 'UTF-8'))
+        f.close()
+
+        try:
+            output = "%s-bin" % f.name
+            cmd = "{compiler} {cflags} {src} -o {out}".format(compiler=compiler, cflags=cflags,
+                                                              src=f.name, out=output)
+            subprocess.check_output(cmd, stderr=subprocess.STDOUT,
+                                    shell=True, universal_newlines=True)
+            os.unlink(output)
+        except subprocess.CalledProcessError as e:
+            enabled = "n"
+
+        os.unlink(f.name)
+        kconfig_add(k, enabled)
+
+def header_dep_test():
+    for k,v in header_deps.items():
+        enabled = "y"
+        source = "#include %s\nint main(int argc, char **argv){\n    return 0;\n}" % v
+        f = tempfile.NamedTemporaryFile(suffix=".c",delete=False)
+        f.write(bytes(source, 'UTF-8'))
+        f.close()
+
+        try:
+            output = "%s-bin" % f.name
+            cmd = "{compiler} {cflags} {src} -o {out}".format(compiler=compiler, cflags=cflags,
+                                                              src=f.name, out=output)
+            subprocess.check_output(cmd, stderr=subprocess.STDOUT,
+                                    shell=True, universal_newlines=True)
+            os.unlink(output)
+        except subprocess.CalledProcessError as e:
+            enabled = "n"
+
+        os.unlink(f.name)
+        kconfig_add(k, enabled)
+
+def exec_discover():
+    for k,v in exec_deps.items():
+        path = which(v) or ""
+        
+        line = "%s ?= %s\n" % (k, path)
+        makefile_gen.append(line)
+
+if __name__ == "__main__":
+    kconfig = "Kconfig.gen"
+    makefile = "Makefile.gen"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--compiler", help="The gcc compiler[for headers based tests]", type=str)
+    parser.add_argument("--cflags", help="Additional cflags[for headers based tests]", type=str)
+    parser.add_argument("--kconfig-output", help="The kconfig fragment output file", type=str)
+    parser.add_argument("--makefile-output", help="The makefile fragment output file", type=str)
+
+    args = parser.parse_args()
+
+    if (args.compiler):
+        compiler = args.compiler
+
+    if (args.cflags):
+        cflags = args.cflags
+
+    if (args.kconfig_output):
+        kconfig = args.kconfig_output
+
+    pkg_config_discover()
+    header_dep_test()
+    fragment_dep_test()
+    exec_discover()
+    python_dep_test()
+
+    fragment = ""
+    for i in kconfig_gen:
+        fragment += "%s\n" % i
+
+    f = open(kconfig, mode="w+")
+    f.write(fragment)
+    f.close()
+
+    fragment = ""
+    for i in makefile_gen:
+        fragment += "%s\n" % i
+
+    f = open(makefile, mode="w+")
+    f.write(fragment)
+    f.close()
---
+++ data/scripts/suite.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+
+# This file is part of the Soletta Project
+#
+# Copyright (C) 2015 Intel Corporation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#   * Neither the name of Intel Corporation nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from multiprocessing import Manager
+from threading import Thread
+import argparse
+import subprocess
+
+WARN = '\033[93m'
+PASS = '\033[92m'
+FAIL = '\033[31m'
+ENDC = '\033[0m'
+
+STATUS_COLOR = [FAIL, PASS]
+STATUS_TAG = ["FAIL", "PASS"]
+
+def run_test_program(cmd, test, stat, log):
+    success = 1
+    print("%sRUNNING: %s%s" % (WARN, ENDC, test))
+    try:
+        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT,
+                                         shell=True, universal_newlines=True)
+        log.append(output)
+    except subprocess.CalledProcessError as e:
+        success = 0
+        log.append(e.output)
+
+    stat[test] = success
+
+    # print out each test's result right away
+    print("%s%s:%s %s" % (STATUS_COLOR[success], STATUS_TAG[success], ENDC, test))
+
+def print_log(log_file, stat, log):
+    output = ""
+    status_cnt = {'FAIL': 0, 'PASS': 0}
+    for k,v in sorted(stat.items()):
+        curr_status = STATUS_TAG[v]
+        status_cnt[curr_status] = status_cnt[curr_status] + 1
+
+    output += "============================================================================\n"
+    output += "Testsuite summary\n"
+    output += "============================================================================\n"
+    output += "# TOTAL: %d\n" % len(stat)
+    output += "# SUCCESS: %d\n" % status_cnt["PASS"]
+    output += "# FAIL: %d\n" % status_cnt["FAIL"]
+    output += "============================================================================\n"
+    output += "See %s\n" % log_file
+    output += "============================================================================\n"
+
+    # show the stat in the stdout
+    print(output)
+
+    log_output = ""
+    f = open(log_file, mode="w+")
+    for i in log:
+        log_output += "%s\n" % i
+    f.write(log_output)
+    f.close()
+
+def run_valgrind_test(args):
+    manager = Manager()
+    common_args = "--error-exitcode=1 --num-callers=30"
+    valgrind_tools = {
+        'memcheck': '--leak-check=full --show-reachable=no',
+        'helgrind': '--history-level=approx',
+        'drd': None,
+        'exp-sgcheck': None,
+    }
+
+    for k,v in valgrind_tools.items():
+        stat = manager.dict()
+        log = manager.list()
+        threads = []
+
+        for i in args.tests.split():
+            cmd = "{valgrind} {test_path} {supp} --tool={tool} {tool_args} {common}". \
+                  format(valgrind=args.valgrind, test_path=i, supp=args.valgrind_supp, \
+                         tool=k, tool_args=v, common=common_args)
+            t = Thread(target=run_test_program, args=(cmd, i, stat, log,))
+            t.start()
+            threads.append(t)
+
+        for t in threads:
+            t.join()
+
+        print_log("test-suite-%s.log" % k, stat, log)
+
+def run_test(args):
+    manager = Manager()
+    stat = manager.dict()
+    log = manager.list()
+    threads = []
+
+    for i in args.tests.split():
+        t = Thread(target=run_test_program, args=(i, i, stat, log,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+    print_log("test-suite.log", stat, log)
+
+def run_suite(args):
+    if args.valgrind:
+        run_valgrind_test(args)
+    else:
+        run_test(args)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tests", help="List of tests to run", type=str)
+    parser.add_argument("--valgrind", help="Path to valgrind, if provided " \
+                        "the tests are run with it", type=str)
+    parser.add_argument("--valgrind-supp", help="Path to valgrind's suppression file", type=str)
+
+    args = parser.parse_args()
+    if args.valgrind_supp:
+        args.valgrind_supp = "--suppressions=%s" % args.valgrind_supp
+
+    run_suite(args)
