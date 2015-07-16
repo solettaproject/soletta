a64d72be1b834c3776db622cb5dce3fb7124a60d
Bruno Dilly <bruno.dilly@intel.com>
2015-07-16 18:15:50 -0300

    docs: show examples on node types doc
    TODO:
      * fix thumbnails (I'm not happy with thumbnails idea, area is too
        small)
      * fix svg visualization: it would be better to move a viewport
        over the image, respecting boundaries. Also it should zoom to
        fit all the area when image is small.
    
    Signed-off-by: Bruno Dilly <bruno.dilly@intel.com>

--- data/scripts/sol-flow-node-type-gen-html.py
+++ data/scripts/sol-flow-node-type-gen-html.py
@@ -35,15 +35,53 @@
 #
 # Example:
 #
-# ./sol-flow-node-type-gen-html.py index.html.in index.html \
+# ./sol-flow-node-type-gen-html.py index.html.in index.html svg_dir/ \
 #   src/lib/flow/builtins.json src/modules/flow/*.json
 
 import json
+import os
 import re
+from os import walk
+
+# TODO - handle types used by conf files
+def get_svgs(svg_dir, path_index):
+    svgs = {}
+    fbp_files = []
+
+    for (dirpath, dirnames, filenames) in walk(svg_dir):
+        for filename in filenames:
+            if filename.endswith(".fbp"):
+                fbp_files.append(os.path.join(dirpath, filename))
+
+    for fbp_file in fbp_files:
+        with open(fbp_file, "r") as f:
+            for line in f:
+                nodes = re.findall("\(.*?\)", line)
+                for node in nodes:
+                    # if it hasn't options, find will return -1, required to
+                    # remove ')'.
+                    type_end = node.find(':')
+                    node = node[1:type_end]
+                    node_files = svgs.get(node, [])
+                    fbp_url = fbp_file[path_index:]
+                    if fbp_url not in node_files:
+                        node_files.append(fbp_url)
+                        svgs[node] = node_files
+
+    return svgs
+
+def print_example(outfile, example):
+    outfile.write("""\
+                    { large:'%(image_path)s', thumb:'%(thumb_path)s', code:'%(code_path)s' },
+""" % {
+    "image_path": example[0:-3] + "svg",
+    "thumb_path": example[0:-3] + "svg",
+    "code_path": example,
+    })
 
 def print_option(outfile, option):
     outfile.write("""\
-                    {name:"%(name)s", description:"%(description)s", dataType:"%(data_type)s", required:true, defaults:"%(default)s"},
+                    { name:"%(name)s", description:"%(description)s", dataType:"%(data_type)s", required:true, defaults:"%(default)s" },
 """ % {
     "name": option["name"],
     "data_type": option["data_type"],
@@ -63,7 +101,7 @@ def print_port(outfile, port):
 group_id = 0
 entry_id = 0
 
-def print_node_type(outfile, node_type):
+def print_node_type(outfile, node_type, svgs):
     global group_id
     global entry_id
     outfile.write("""\
@@ -113,11 +151,10 @@ def print_node_type(outfile, node_type):
                 exampleList:[
 """)
 
-    # TODO add real examples
-    outfile.write("""\
-                    {large:'images/checker.jpg',thumb:'images/checker_thumb.jpg',code:'example 0 code here'},
-                    {large:'images/checker.jpg',thumb:'images/checker_thumb.jpg',code:'example 8 code here'}
-""")
+    examples = svgs.get(node_type["name"])
+    if examples:
+        for example in examples:
+            print_example(outfile, example)
 
     outfile.write("""\
                 ],
@@ -129,7 +166,7 @@ def print_node_type(outfile, node_type):
 """)
     entry_id = entry_id + 1
 
-def print_description(outfile, description, infile):
+def print_description(outfile, description, infile, svgs):
     global group_id
     global entry_id
 
@@ -155,7 +192,7 @@ def print_description(outfile, description, infile):
 
         entry_id = 0
         for node_type in description[module]:
-            print_node_type(outfile, node_type)
+            print_node_type(outfile, node_type, svgs)
 
         outfile.write("""\
             $(category.getContents()).isotope({
@@ -168,15 +205,22 @@ def print_description(outfile, description, infile):
 
 PLACEHOLDER = "<!-- PLACEHOLDER -->\n"
 
-def create_nodes(outfile, infiles):
+def create_nodes(outfile, infiles, svgs):
     for infile in infiles:
         description = json.load(infile)
-        print_description(outfile, description, infile)
+        print_description(outfile, description, infile, svgs)
 
-def create_doc(template, outfile, infiles):
+def create_doc(template, outfile, svg_dir, infiles):
+    base = os.path.dirname(outfile.name)
+    base_start = svg_dir.find(base)
+    if base_start < 0:
+        print("Warning: svg dir (%s) isn't inside html path (%s)" %
+              svg_dir, outfile)
+    svgs = get_svgs(svg_dir, base_start + len(base) + 1)
+
     for line in template.readlines():
         if (line == PLACEHOLDER):
-            create_nodes(outfile, infiles)
+            create_nodes(outfile, infiles, svgs)
         else:
             outfile.write(line)
 
@@ -190,13 +234,16 @@ if __name__ == "__main__":
     parser.add_argument("html",
                         help="Path to write html file",
                         type=argparse.FileType('w'))
+    parser.add_argument("svg_dir",
+                        help="Path to directory with svg and fbp example files",
+                        type=str)
     parser.add_argument("inputs_list", nargs='+',
                         help="List of input description files in JSON format",
                         type=argparse.FileType('r'))
 
     args = parser.parse_args()
 
-    create_doc(args.template, args.html, args.inputs_list)
+    create_doc(args.template, args.html, args.svg_dir, args.inputs_list)
 
     args.template.close()
     args.html.close()
