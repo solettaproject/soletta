7a0757a4ed8eecca1a171f61915d52eea2b611cf
Gustavo Sverzut Barbieri <gustavo.barbieri@intel.com>
2015-07-29 16:01:33 -0300

    sol_platform_linux_fork_run: review and document.
    Review sol_platform_linux_fork_run code and document it, the following
    were done:
    
     * errno is properly set.
    
     * functions return 0 or -errno, not void.
    
     * find_handle() is used to see if the handle is valid and alive,
       avoiding us to access invalid pointers if user call functions on
       pointers that were already dead.
    
     * sol_platform_linux_fork_run_get_pid() uses uint64_t and UINT64_MAX
       for errors.
    
     * sol_platform_linux_fork_run_stop() now handles waitpid() (EINTR,
       ECHILD...)
    
     * added sol_platform_linux_fork_run_exit() variant to make clear what
       child should call in order to exit (instead of plain exit()). This
       should address the problems noticed by Thiago Macieira
       (@thiagomacieira).
    
     * added helper sol_platform_linux_fork_run_send_signal().
    
    Signed-off-by: Gustavo Sverzut Barbieri gustavo.barbieri@intel.com

--- src/lib/common/sol-platform-linux.h
+++ src/lib/common/sol-platform-linux.h
@@ -33,6 +33,7 @@
 #pragma once
 
 #include "sol-platform.h"
+#include "sol-macros.h"
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -41,9 +42,122 @@
 extern "C" {
 #endif
 
+/**
+ * Fork a new process and run the given @a on_fork callback on that process.
+ *
+ * This call will execute fork(), then wait for children to be ready
+ * (internally uses a pipe() to synchronize), reset all signal
+ * handlers to their default values and then call @a on_fork. This
+ * user-provided function may then do whatever it wants and when it
+ * returns the process will exit with @c EXIT_SUCCESS. The other way
+ * to exit is to call sol_platform_linux_fork_run_exit().
+ *
+ * @note whenever the user wants to exit with a different code or
+ *       prematurely, never call exit() directly, instead call
+ *       sol_platform_linux_fork_run_exit() (or _exit()). The reason
+ *       is that functions registered with atexit() or on_exit()
+ *       shouldn't be executed in the children processes.
+ *
+ * When the child exits, the user-provided @a on_child_exit is called
+ * and after this point the handle returned by
+ * sol_platform_linux_fork_run() is invalidated (freed), thus
+ * shouldn't be used any further.
+ *
+ * The main process may force the child process to stop using
+ * sol_platform_linux_fork_run_stop(), that blocking call will send @c
+ * SIGTERM and wait the child to exit using waitpid(), calling @a
+ * on_child_exit before it returns.
+ *
+ * @param on_fork the function to call back after the child process is
+ *        ready. The given data is the same pointer given as @a data.
+ * @param on_child_exit the function to call back on the main
+ *        (original) process when the child exits. The given data is
+ *        the same pointer given as @a data, @c pid is the process
+ *        identifier (as returned by
+ *        sol_platform_linux_fork_run_get_pid()) and the status is the
+ *        one returned by libc waitpid(), that means the macros such
+ *        as WIFEXITED(), WEXITSTATUS(), WIFSIGNALED(), WTERMSIG() and
+ *        similar from @c sys/wait.h should be used to interpret it.
+ * @param data the context data to give to the callbacks @a on_fork
+ *        and @a on_child_exit. It is not modified in any way by
+ *        sol_platform_linux_fork_run functions.
+ *
+ * @return the handle to the child process or @c NULL on errors (and
+ *         @c errno is set to the actual reason). The handle may be
+ *         used to query the process identifier, to stop the process.
+ *
+ * @see sol_platform_linux_fork_run_stop()
+ * @see sol_platform_linux_fork_run_get_pid()
+ * @see sol_platform_linux_fork_run_exit()
+ */
 struct sol_platform_linux_fork_run *sol_platform_linux_fork_run(void (*on_fork)(void *data), void (*on_child_exit)(void *data, uint64_t pid, int status), const void *data);
-void sol_platform_linux_fork_run_stop(struct sol_platform_linux_fork_run *handle);
-pid_t sol_platform_linux_fork_run_get_pid(const struct sol_platform_linux_fork_run *handle);
+
+/**
+ * Force the child process to stop running and wait for it.
+ *
+ * This is a blocking function that will kill the child process using
+ * @c SIGTERM, then waiting the process to exit using waitpid().
+ *
+ * After the process exit, the @c on_child_exit is called and the
+ * given @a handle is invalidated (freed) before this function
+ * returns.
+ *
+ * If the user doesn't want the blocking behavior he may use
+ * sol_platform_linux_fork_run_send_signal() using @c SIGTERM and wait
+ * @c on_child_exit to be called when the process exits.
+ *
+ * @param handle a valid (non-NULL and alive) handle returned by
+ *        sol_platform_linux_fork_run() to be terminated. After this
+ *        function return, the handle is then invalidated.
+ * @return 0 on success or -errno on failure.
+ *
+ * @see sol_platform_linux_fork_run_send_signal()
+ */
+int sol_platform_linux_fork_run_stop(struct sol_platform_linux_fork_run *handle);
+
+
+/**
+ * Send a signal to the child process.
+ *
+ * This is a helper using sol_platform_linux_fork_run_get_pid() and
+ * kill(). It may be useful to send control signals such as @c SIGUSR1
+ * and @c SIGUSR2.
+ *
+ * @param handle a valid (non-NULL and alive) handle returned by
+ *        sol_platform_linux_fork_run().
+ * @param sig the signal number, such as @c SIGUSR1.
+ * @return 0 on success or -errno on failure.
+ */
+int sol_platform_linux_fork_run_send_signal(struct sol_platform_linux_fork_run *handle, int sig);
+
+/**
+ * The process identifier of this child.
+ *
+ * @param handle a valid (non-NULL and alive) handle returned by
+ *        sol_platform_linux_fork_run().
+ *
+ * @return the processes identifier (PID) that can be used with
+ *         syscalls such as kill(). On errors, UINT64_MAX is returned
+ *         and errno is set to @c ENOENT.
+ */
+uint64_t sol_platform_linux_fork_run_get_pid(const struct sol_platform_linux_fork_run *handle);
+
+/**
+ * Exit from a child process.
+ *
+ * This function is to be called from inside @c on_fork to exit the
+ * child process using a specific status, such as @c EXIT_FAILURE or
+ * @c EXIT_SUCCESS.
+ *
+ * Children process should never call exit() directly, rather use this
+ * function or _exit().
+ *
+ * @param status the value to use to exit this process, it will be
+ *        available to the main process via the @c on_child_exit @c
+ *        status parameter, in that case use the @c sys/wait.h macros
+ *        to get the actual value, such as WEXITSTATUS().
+ */
+void sol_platform_linux_fork_run_exit(int status) SOL_ATTR_NORETURN;
 
 #ifdef __cplusplus
 }
