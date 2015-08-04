89e2947ade1efd1b09fc1a37a9a38f33221f4d6e
José Roberto de Souza <jose.souza@intel.com>
2015-08-04 18:11:15 -0300

    squash: Port magnetometer lsm303
    Ederson need to review.

--- src/modules/flow/magnetometer/lsm303.c
+++ src/modules/flow/magnetometer/lsm303.c
@@ -34,6 +34,7 @@
 #include "sol-flow-internal.h"
 
 #include <sol-i2c.h>
+#include <sol-mainloop.h>
 #include <sol-util.h>
 
 #include <errno.h>
@@ -44,14 +45,33 @@
 #define LSM303_MAG_REG_MR_REG_M 0x02
 #define LSM303_ACCEL_REG_OUT_X_H_M 0x03
 
+#define MAG_STEP_TIME 1
+
 struct magnetometer_lsm303_data {
+    struct sol_flow_node *node;
     struct sol_i2c *i2c;
-    double reading[3];
+    void *i2c_pending;
+    struct sol_timeout *timer;
     uint16_t gain_xy;
     uint16_t gain_z;
     uint8_t slave;
     uint8_t scale;
+    uint8_t ready : 1;
+    unsigned pending_ticks;
+    uint8_t i2c_buffer[LSM303_MAG_BYTES_NUMBER];
+    double reading[3];
 };
+
+static bool magnetometer_lsm303_tick_do(void *data);
+
+static int
+timer_sched(struct magnetometer_lsm303_data *mdata, unsigned int timeout_ms, bool (*cb)(void *data))
+{
+    mdata->timer = sol_timeout_add(timeout_ms, cb, mdata);
+    SOL_NULL_CHECK(mdata->timer, -ENOMEM);
+
+    return 0;
+}
 
 static bool
 _get_range_bits_and_gain(double range, uint8_t *range_bit, uint16_t *gain_xy, uint16_t *gain_z)
@@ -92,22 +112,83 @@ _get_range_bits_and_gain(double range, uint8_t *range_bit, uint16_t *gain_xy, ui
     return true;
 }
 
+static void
+i2c_write_range_cb(void *cb_data, struct sol_i2c *i2c, uint8_t reg, uint8_t *data, ssize_t status)
+{
+    struct magnetometer_lsm303_data *mdata = cb_data;
+
+    mdata->i2c_pending = NULL;
+    if (status < 0) {
+        SOL_WRN("Could not set LSM303 magnetometer range");
+        return;
+    }
+
+    mdata->ready = true;
+    if (mdata->pending_ticks)
+        magnetometer_lsm303_tick_do(mdata);
+}
+
+static void
+i2c_write_mode_cb(void *cb_data, struct sol_i2c *i2c, uint8_t reg, uint8_t *data, ssize_t status)
+{
+    struct magnetometer_lsm303_data *mdata = cb_data;
+
+    mdata->i2c_pending = NULL;
+    if (status < 0) {
+        SOL_WRN("Could not enable LSM303 magnetometer");
+        return;
+    }
+
+    if (!_get_range_bits_and_gain(mdata->scale, mdata->i2c_buffer,
+        &mdata->gain_xy, &mdata->gain_z)) {
+        SOL_WRN("Invalid gain. Expected one of 1.3, 1.9, 2.5, 4.0, 4.5, 5.6 or 8.1");
+        return;
+    }
+
+    mdata->i2c_pending = sol_i2c_write_register(mdata->i2c,
+        LSM303_MAG_REG_CRB_REG_M, mdata->i2c_buffer, 1, i2c_write_range_cb,
+        mdata);
+    if (!mdata->i2c_pending)
+        SOL_WRN("Could not set LSM303 magnetometer range");
+
+    return;
+}
+
+static bool
+lsm303_init(void *data)
+{
+    struct magnetometer_lsm303_data *mdata = data;
+
+    mdata->timer = NULL;
+    if (sol_i2c_busy(mdata->i2c)) {
+        timer_sched(mdata, MAG_STEP_TIME, lsm303_init);
+        return false;
+    }
+
+    if (!sol_i2c_set_slave_address(mdata->i2c, mdata->slave)) {
+        SOL_WRN("Failed to set slave at address 0x%02x\n", mdata->slave);
+        return false;
+    }
+
+    mdata->i2c_buffer[0] = LSM303_MAG_DEFAULT_MODE;
+    mdata->i2c_pending = sol_i2c_write_register(mdata->i2c,
+        LSM303_MAG_REG_MR_REG_M, mdata->i2c_buffer, 1, i2c_write_mode_cb,
+        mdata);
+    if (!mdata->i2c_pending)
+        SOL_WRN("Could not enable LSM303 magnetometer");
+
+    return false;
+}
+
 static int
 magnetometer_lsm303_open(struct sol_flow_node *node, void *data, const struct sol_flow_node_options *options)
 {
     struct magnetometer_lsm303_data *mdata = data;
     const struct sol_flow_node_type_magnetometer_lsm303_options *opts;
-    static const uint8_t mode = LSM303_MAG_DEFAULT_MODE;
-    uint8_t range_bit;
 
     SOL_FLOW_NODE_OPTIONS_SUB_API_CHECK(options, SOL_FLOW_NODE_TYPE_MAGNETOMETER_LSM303_OPTIONS_API_VERSION,
         -EINVAL);
     opts = (const struct sol_flow_node_type_magnetometer_lsm303_options *)options;
-
-    if (!_get_range_bits_and_gain(opts->scale.val, &range_bit, &mdata->gain_xy, &mdata->gain_z)) {
-        SOL_WRN("Invalid gain. Expected one of 1.3, 1.9, 2.5, 4.0, 4.5, 5.6 or 8.1");
-        return -EINVAL;
-    }
 
     mdata->i2c = sol_i2c_open(opts->i2c_bus.val, SOL_I2C_SPEED_10KBIT);
     if (!mdata->i2c) {
@@ -115,28 +196,12 @@ magnetometer_lsm303_open(struct sol_flow_node *node, void *data, const struct so
         return -EINVAL;
     }
 
-    if (!sol_i2c_set_slave_address(mdata->i2c, opts->i2c_slave.val)) {
-        SOL_WRN("Failed to set slave at address 0x%02x\n",
-            opts->i2c_slave.val);
-        goto fail;
-    }
     mdata->slave = opts->i2c_slave.val;
+    mdata->scale = opts->scale.val;
+    mdata->node = node;
 
-    if (!sol_i2c_write_register(mdata->i2c, LSM303_MAG_REG_MR_REG_M, &mode, 1)) {
-        SOL_WRN("Could not enable LSM303 magnetometer");
-        goto fail;
-    }
-
-    if (!sol_i2c_write_register(mdata->i2c, LSM303_MAG_REG_CRB_REG_M, &range_bit, 1)) {
-        SOL_WRN("Could not set LSM303 magnetometer range");
-        goto fail;
-    }
-
+    lsm303_init(mdata);
     return 0;
-
-fail:
-    sol_i2c_close(mdata->i2c);
-    return -EINVAL;
 }
 
 static void
@@ -144,47 +209,43 @@ magnetometer_lsm303_close(struct sol_flow_node *node, void *data)
 {
     struct magnetometer_lsm303_data *mdata = data;
 
+    if (mdata->i2c_pending)
+        sol_i2c_pending_cancel(mdata->i2c, mdata->i2c_pending);
     if (mdata->i2c)
         sol_i2c_close(mdata->i2c);
+
+    if (mdata->timer)
+        sol_timeout_del(mdata->timer);
 }
 
 static void
-_lsm303_send_output_packets(struct sol_flow_node *node, struct magnetometer_lsm303_data *mdata)
+_lsm303_send_output_packets(struct magnetometer_lsm303_data *mdata)
 {
     struct sol_direction_vector val =
     {
         .min = -mdata->scale,
-        .max = -mdata->scale,
+        .max = mdata->scale,
         .x = mdata->reading[0],
         .y = mdata->reading[1],
         .z = mdata->reading[2]
     };
 
-    sol_flow_send_direction_vector_packet(node,
+    sol_flow_send_direction_vector_packet(mdata->node,
         SOL_FLOW_NODE_TYPE_MAGNETOMETER_LSM303__OUT__OUT, &val);
 }
 
-static int
-magnetometer_lsm303_tick(struct sol_flow_node *node, void *data, uint16_t port, uint16_t conn_id, const struct sol_flow_packet *packet)
+static void
+i2c_lsm303_read_data_cb(void *cb_data, struct sol_i2c *i2c, uint8_t reg, uint8_t *data, ssize_t status)
 {
-    struct magnetometer_lsm303_data *mdata = data;
-    int8_t buffer[LSM303_MAG_BYTES_NUMBER];
-    int r;
+    struct magnetometer_lsm303_data *mdata = cb_data;
+    uint8_t *buffer = mdata->i2c_buffer;
 
-    if (!sol_i2c_set_slave_address(mdata->i2c, mdata->slave)) {
-        const char errmsg[] = "Failed to set slave at address 0x%02x";
-        SOL_WRN(errmsg, mdata->slave);
-        sol_flow_send_error_packet(node, EIO, errmsg, mdata->slave);
-        return -EIO;
-    }
-
-    r = sol_i2c_read_register(mdata->i2c, LSM303_ACCEL_REG_OUT_X_H_M,
-        (uint8_t *)buffer, sizeof(buffer));
-    if (r <= 0) {
+    mdata->i2c_pending = NULL;
+    if (status < 0) {
         const char errmsg[] = "Failed to read LSM303 magnetometer samples";
         SOL_WRN(errmsg);
-        sol_flow_send_error_packet(node, EIO, errmsg);
-        return -EIO;
+        sol_flow_send_error_packet(mdata->node, EIO, errmsg);
+        return;
     }
 
     /* Get X, Z and Y. That's why reading[] is indexed 0, 2 and 1. */
@@ -192,8 +253,54 @@ magnetometer_lsm303_tick(struct sol_flow_node *node, void *data, uint16_t port, 
     mdata->reading[2] = ((buffer[2] << 8) | buffer[3]) / mdata->gain_z;
     mdata->reading[1] = ((buffer[4] << 8) | buffer[5]) / mdata->gain_xy;
 
-    _lsm303_send_output_packets(node, mdata);
+    _lsm303_send_output_packets(mdata);
 
+    mdata->pending_ticks--;
+    if (mdata->pending_ticks)
+        magnetometer_lsm303_tick_do(mdata);
+}
+
+static bool
+magnetometer_lsm303_tick_do(void *data)
+{
+    struct magnetometer_lsm303_data *mdata = data;
+
+    mdata->timer = NULL;
+    if (sol_i2c_busy(mdata->i2c)) {
+        timer_sched(mdata, MAG_STEP_TIME, magnetometer_lsm303_tick_do);
+        return false;
+    }
+
+    if (!sol_i2c_set_slave_address(mdata->i2c, mdata->slave)) {
+        const char errmsg[] = "Failed to set slave at address 0x%02x";
+        SOL_WRN(errmsg, mdata->slave);
+        sol_flow_send_error_packet(mdata->node, EIO, errmsg, mdata->slave);
+        return false;
+    }
+
+    mdata->i2c_pending = sol_i2c_read_register(mdata->i2c,
+        LSM303_ACCEL_REG_OUT_X_H_M, mdata->i2c_buffer,
+        sizeof(mdata->i2c_buffer), i2c_lsm303_read_data_cb, mdata);
+    if (!mdata->i2c_pending) {
+        const char errmsg[] = "Failed to read LSM303 magnetometer samples";
+        SOL_WRN(errmsg);
+        sol_flow_send_error_packet(mdata->node, EIO, errmsg);
+    }
+
+    return false;
+}
+
+static int
+magnetometer_lsm303_tick(struct sol_flow_node *node, void *data, uint16_t port, uint16_t conn_id, const struct sol_flow_packet *packet)
+{
+    struct magnetometer_lsm303_data *mdata = data;
+
+    if (!mdata->ready || mdata->pending_ticks) {
+        mdata->pending_ticks++;
+        return 0;
+    }
+
+    magnetometer_lsm303_tick_do(mdata);
     return 0;
 }
 
