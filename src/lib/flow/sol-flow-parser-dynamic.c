ded44e1387b32db37161abe1cd0629657a0fa266
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2015-08-21 12:04:06 -0300

    flow: add support for metatype modules
    Use the "module" build infrastructure for finding metatypes to be used
    when processing "DECLARE=..." statements in an FBP file.
    
    These modules will be in modules/flow-metatype/ and they should follow
    the interface defined by sol-flow-metatype.h, in particular use the
    macro SOL_FLOW_METATYPE(). Currently the only job of such module is to
    create a new type given a contents (usually a filename).
    
    When running with dynamic libraries enabled, the flow parser will use
    the library loader to look for modules that implement metatypes, so the
    loaded metatypes are cached. As expected, if there are builtin types,
    those are loaded too.
    
    For now, both FBP and JS are handled "outside" this infrastructure. FBP
    will remain special for a while, but JS will be soon moved to be a
    module.
    
    New types using those metatypes can also be created directly from C
    code, by using sol_flow_parse_*_metatype() functions. This reduces the
    need of a "per metatype" C API. This is implemented providing to the
    metatype a "read_file" a special read_file function that can read only
    one file, the one provided by the buffer.
    
    Signed-off-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

---
+++ src/lib/flow/sol-flow-parser-dynamic.c
@@ -0,0 +1,89 @@
+/*
+ * This file is part of the Soletta Project
+ *
+ * Copyright (C) 2015 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "sol-flow-internal.h"
+#include "sol-lib-loader.h"
+
+static struct sol_lib_loader *metatype_loader;
+
+static bool
+check_metatype(const char *path, const char *symbol_name, void *symbol)
+{
+    const struct sol_flow_metatype **p_metatype, *metatype;
+
+    p_metatype = symbol;
+    metatype = *p_metatype;
+
+    if (!metatype) {
+        SOL_WRN("Symbol '%s' in module '%s' is points to NULL instead of a valid metatype",
+            symbol_name, path);
+        return false;
+    }
+
+    if (metatype->api_version != SOL_FLOW_METATYPE_API_VERSION) {
+        SOL_WRN("Module '%s' has incorrect api_version: %u expected %u",
+            path, metatype->api_version, SOL_FLOW_METATYPE_API_VERSION);
+        return false;
+    }
+
+    return true;
+}
+
+sol_flow_metatype_create_type_func
+get_dynamic_create_type_func(const struct sol_str_slice name)
+{
+    const struct sol_flow_metatype *metatype;
+    void *symbol;
+    char name_str[PATH_MAX] = {};
+
+    SOL_INT_CHECK(name.len, > (PATH_MAX - 1), NULL);
+    memcpy(name_str, name.data, name.len);
+
+    if (!metatype_loader) {
+        metatype_loader = sol_lib_loader_new_in_rootdir(
+            FLOWMETATYPEMODULESDIR, "SOL_FLOW_METATYPE", check_metatype);
+        SOL_NULL_CHECK(metatype_loader, NULL);
+    }
+
+    symbol = sol_lib_load(metatype_loader, name_str);
+    if (!symbol)
+        return NULL;
+
+    metatype = *(const struct sol_flow_metatype **)symbol;
+    return metatype->create_type;
+}
+
+void
+loaded_metatype_cache_shutdown(void)
+{
+    sol_lib_loader_del(metatype_loader);
+}
