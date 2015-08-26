31a55201d72c84e4a8e91953f98ce151f806d073
Ederson de Souza <ederson.desouza@intel.com>
2015-08-26 17:45:26 -0300

    flow: fs node transform into persistence node - with efivars
    fs node renamed to persistence node. As it will not only deal with fs
    anymore. Besides it, a new option was added: 'storage', which defines
    storage destination, currently 'fs' and 'efivars'. When using 'fs',
    'name' can be a fully fledged path to the file where value will be
    persisted.
    'efivars' saves on 'efivarfs', using a reasonable attributes set:
    "EFI_VARIABLE_NON_VOLATILE | EFI_VARIABLE_BOOTSERVICE_ACCESS |
    EFI_VARIABLE_RUNTIME_ACCESS".
    'fs' now creates files in case they don't happen to exist.
    When there is no previous persisted value, node 'default_value' will
    be used instead.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

---
+++ src/modules/flow/persistence/fs-storage.c
@@ -0,0 +1,100 @@
+/*
+ * This file is part of the Soletta Project
+ *
+ * Copyright (C) 2015 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "fs-storage.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "sol-buffer.h"
+#include "sol-log.h"
+#include "sol-util.h"
+#include "sol-util-file.h"
+
+int
+fs_write(const char *name, struct sol_buffer *buffer)
+{
+    FILE *file;
+
+    file = fopen(name, "w+e");
+    if (!file) {
+        SOL_WRN("Could not open persistence file [%s]", name);
+        return -errno;
+    }
+
+    fwrite(buffer->data, buffer->used, 1, file);
+    if (ferror(file)) {
+        SOL_WRN("Could not write to persistence file [%s]", name);
+        return -EIO;
+    }
+
+    if (fclose(file)) {
+        SOL_WRN("Could not close persistence file [%s]", name);
+        return -errno;
+    }
+
+    return 0;
+}
+
+int
+fs_read(const char *name, struct sol_buffer *buffer)
+{
+    int r, fd;
+
+    fd = open(name, O_RDONLY | O_CLOEXEC);
+    if (fd < 0) {
+        SOL_WRN("Could not open persistence file [%s]", name);
+        return -errno;
+    }
+
+    if (buffer->capacity) {
+        r = sol_util_fill_buffer(fd, buffer, buffer->capacity);
+    } else {
+        size_t size = 0;
+        char *data = sol_util_load_file_fd_string(fd, &size);
+        if (data) {
+            buffer->capacity = size;
+            buffer->used = size;
+            buffer->data = data;
+            r = size;
+        } else
+            r = -errno;
+    }
+
+    close(fd);
+
+    return r;
+}
