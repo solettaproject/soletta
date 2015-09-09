1ba0065618e7b3985c230f3552b3dd3798981bb5
Flavio Ceolin <flavio.ceolin@gmail.com>
2015-09-09 17:49:28 -0300

    json: Add helper function to convert double to string
    Takes in consideration the locale.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- src/lib/parsers/include/sol-json.h
+++ src/lib/parsers/include/sol-json.h
@@ -415,6 +415,8 @@ char *sol_json_escape_string(const char *str, char *buf, size_t len) SOL_ATTR_NO
         sol_json_escape_string(s, alloca(str_len ## __COUNT__), str_len ## __COUNT__); \
     })
 
+int sol_json_double_to_str(const double value, char *buf, size_t buf_len);
+
 /**
  * @}
  */
