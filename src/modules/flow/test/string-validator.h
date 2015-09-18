f6de759e34d8f88d6af1ca0498c594eb2f2b4e06
Ederson de Souza <ederson.desouza@intel.com>
2015-09-18 17:35:28 -0300

    flow/test: string-validator node type
    So, we can test if some node outputs a given sequence of strings.
    
    Signed-off-by: Ederson de Souza <ederson.desouza@intel.com>

--- src/modules/flow/test/string-validator.h
+++ src/modules/flow/test/string-validator.h
@@ -30,26 +30,16 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <float.h>
-
-
-#include "sol-util.h"
-#include "sol-log-internal.h"
-
 #include "test-module.h"
+#include "sol-vector.h"
 
-#include "sol-flow/test.h"
+struct string_validator_data {
+    bool done;
+    char *sequence;
+    uint16_t next_index;
+    struct sol_vector values;
+};
 
-SOL_LOG_INTERNAL_DECLARE(_test_log_domain, "flow-test");
-
-#include "result.h"
-#include "boolean-generator.h"
-#include "boolean-validator.h"
-#include "float-generator.h"
-#include "float-validator.h"
-#include "int-validator.h"
-#include "int-generator.h"
-#include "blob-validator.h"
-#include "string-validator.h"
-
-#include "test-gen.c"
+DECLARE_OPEN_FUNCTION(string_validator_open);
+DECLARE_CLOSE_FUNCTION(string_validator_close);
+DECLARE_PROCESS_FUNCTION(string_validator_process);
