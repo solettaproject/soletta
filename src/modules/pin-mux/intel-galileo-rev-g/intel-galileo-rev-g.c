0316dd1be9d5a0c5ee34d61e598f17bb6bc86c56
Murilo Belluzzo <murilo.belluzzo@intel.com>
2015-09-23 15:36:14 -0300

    pin-mux: Keep track of open gpios...
    ...so struct sol_gpio won't leak and we can close them on shutdown.
    
    We were avoiding to do this since introduces extra complexity on code
    that should be straight and simple.
    
    Primary reason is that we cannot close those gpios after usage
    because they lose the state and multiplexer does not work. So at first
    we were leaving them open since there wasn't any major drawbacks.
    
    But, by doing this, 'sol_gpio' structures started to leak what bring
    us to this fix :(
    
    Also took the opportunity to add some missing 'mux' namespace on
    intel-common.{c,h}.
    
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- src/modules/pin-mux/intel-galileo-rev-g/intel-galileo-rev-g.c
+++ src/modules/pin-mux/intel-galileo-rev-g/intel-galileo-rev-g.c
@@ -317,29 +317,30 @@ static struct mux_controller pwm_controller_list[] = {
 static int
 _set_aio(const int device, const int pin)
 {
-    return set_aio(device, pin, aio_controller_list, (int)ARRAY_SIZE(aio_controller_list));
+    return mux_set_aio(device, pin, aio_controller_list, (int)ARRAY_SIZE(aio_controller_list));
 }
 
 static int
 _set_gpio(const int pin, const enum sol_gpio_direction dir)
 {
-    return set_gpio(pin, dir, gpio_dev_0, (int)ARRAY_SIZE(gpio_dev_0));
+    return mux_set_gpio(pin, dir, gpio_dev_0, (int)ARRAY_SIZE(gpio_dev_0));
 }
 
 static int
 _set_i2c(const uint8_t bus)
 {
-    return set_i2c(bus, i2c_dev_0, ARRAY_SIZE(i2c_dev_0));
+    return mux_set_i2c(bus, i2c_dev_0, ARRAY_SIZE(i2c_dev_0));
 }
 
 static int
 _set_pwm(const int device, const int channel)
 {
-    return set_pwm(device, channel, pwm_controller_list, (int)ARRAY_SIZE(pwm_controller_list));
+    return mux_set_pwm(device, channel, pwm_controller_list, (int)ARRAY_SIZE(pwm_controller_list));
 }
 
 SOL_PIN_MUX_DECLARE(INTEL_GALILEO_REV_G,
     .plat_name = "intel-galileo-rev-g",
+    .shutdown = mux_shutdown,
     .aio = _set_aio,
     .gpio = _set_gpio,
     .i2c = _set_i2c,
