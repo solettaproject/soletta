9ccb878985260ce2c51a9323e70ff58e77f11b52
Flavio Ceolin <flavio.ceolin@gmail.com>
2015-09-30 17:16:48 -0300

    http: Helper to escape strings
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- src/lib/comms/sol-http-common.c
+++ src/lib/comms/sol-http-common.c
@@ -31,6 +31,8 @@
  */
 
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "sol-http.h"
 #include "sol-log.h"
@@ -72,4 +74,52 @@ sol_http_param_free(struct sol_http_param *params)
         return;
     }
     sol_vector_clear(&params->params);
+}
+
+SOL_API int
+sol_http_escape_string(char **escaped, const char *value)
+{
+    int r;
+    size_t value_len;
+    struct sol_buffer buf = SOL_BUFFER_INIT_EMPTY;
+
+    SOL_NULL_CHECK(value, -EINVAL);
+    SOL_NULL_CHECK(escaped, -EINVAL);
+
+    value_len = strlen(value);
+
+    while (value_len--) {
+        unsigned char c = *value;
+        switch (c) {
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        case 'a': case 'b': case 'c': case 'd': case 'e':
+        case 'f': case 'g': case 'h': case 'i': case 'j':
+        case 'k': case 'l': case 'm': case 'n': case 'o':
+        case 'p': case 'q': case 'r': case 's': case 't':
+        case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
+        case 'A': case 'B': case 'C': case 'D': case 'E':
+        case 'F': case 'G': case 'H': case 'I': case 'J':
+        case 'K': case 'L': case 'M': case 'N': case 'O':
+        case 'P': case 'Q': case 'R': case 'S': case 'T':
+        case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+        case '_': case '~': case '.': case '-':
+            r = sol_buffer_append_char(&buf, c);
+            SOL_INT_CHECK_GOTO(r, < 0, end);
+            break;
+        default:
+            r = sol_buffer_append_printf(&buf, "%%%02X", c);
+            SOL_INT_CHECK_GOTO(r, < 0, end);
+        }
+        value++;
+    }
+
+    r = sol_buffer_ensure_nul_byte(&buf);
+    SOL_INT_CHECK_GOTO(r, < 0, end);
+
+    *escaped = sol_buffer_steal(&buf, NULL);
+
+end:
+    sol_buffer_fini(&buf);
+    return r;
 }
