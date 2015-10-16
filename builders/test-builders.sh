b1a920a7904fce20e79ff19795078fab77151174
Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>
2015-10-16 15:13:54 -0300

    builders: test script now tries to compile test programs
    The script test-builders.sh now uses the 'sol' tool to compile the test
    programs. This can be skipped using the '-s' flag.
    
    Signed-off-by: Caio Marcelo de Oliveira Filho <caio.oliveira@intel.com>

--- builders/test-builders.sh
+++ builders/test-builders.sh
@@ -43,12 +43,15 @@ OPTIONS
      -b             soletta target branch.
      -h             soletta host repo.
      -r             soletta host branch.
+     -s             skip testing the `sol` tool and builds.
      -p             show this help.
 
      Host and target repos default to this soletta repo, e.g. ../, while
      host and target branches default to the current checked out branch.
 " 1>&$1
 }
+
+OPT_SKIP_SOL_BUILDS=0
 
 while getopts "t:b:h:r:p" o; do
     case "${o}" in
@@ -60,6 +63,8 @@ while getopts "t:b:h:r:p" o; do
             export SOLETTA_HOST_REPO="$OPTARG";;
         r)
             export SOLETTA_HOST_BRANCH="$OPTARG";;
+	s)
+	    export OPT_TEST_SOL_BUILDS=1;;
         p)
             usage 1; exit 0;;
         \?)
@@ -75,8 +80,75 @@ if [[ -z "$SOLETTA_HOST_REPO" ]]; then
     export SOLETTA_HOST_REPO="$SCRIPT_DIR/.."
 fi
 
-for dir in $SCRIPT_DIR/*/; do
-    if [[ $dir =~ platform-* ]]; then
-        $dir/prepare
+if [ $OPT_SKIP_SOL_BUILDS -eq 0 ]; then
+    command -v go > /dev/null
+    if [ $? -eq 1 ]; then
+	echo "Couldn't find go to build 'sol' tool. You can skip this by running with '-s' option." >&2
+	exit 1
+    fi
+
+    pushd $SCRIPT_DIR/sol > /dev/null
+    go build
+    if [ $? -eq 1 ]; then
+	echo "Failed build 'sol' tool." >&2
+    fi
+    popd > /dev/null
+fi
+
+for dir in $SCRIPT_DIR/platform-*/; do
+    $dir/prepare
+
+    if [ $? -ne 0 ]; then
+	echo "Failed to prepare $dir" >&2
+	exit 1
     fi
 done
+
+if [ $OPT_SKIP_SOL_BUILDS -eq 1 ]; then
+    echo "Skipped building 'sol' tool and using it to build the test programs."
+    exit
+fi
+
+# Use an alternative port to avoid conflict with any other manually
+# ran sol server.
+SOL_TOOL="$SCRIPT_DIR/sol/sol -addr=localhost:2223"
+
+SERVER_OUTPUT=$(mktemp)
+
+echo
+echo "=== Running build server, output in: $SERVER_OUTPUT"
+echo
+
+pushd $SCRIPT_DIR/out > /dev/null
+$SOL_TOOL -run-as-server 2>&1 > $SERVER_OUTPUT &
+
+SERVER_PID=$!
+popd > /dev/null
+
+trap 'kill $SERVER_PID' EXIT
+
+# Skip the platform-test since it always fail to compile, it's goal is
+# to show the environment things would be compiled.
+
+PLATFORMS=$($SOL_TOOL | grep -v 'platform-test')
+echo $PLATFORMS | tr ' ' '\n'
+
+for test in $SCRIPT_DIR/tests/*; do
+    pushd $test > /dev/null
+    for plat in $PLATFORMS; do
+	echo
+	echo "=== Using 'sol' tool to build '$test' in platform '$plat'"
+	echo
+	$SOL_TOOL $plat
+	if [ $? -eq 1 ]; then
+	    echo "Failed building '$test' in platform '$plat'" >&2
+	    exit 1
+	fi
+	# TODO: Check the actual build product!
+    done
+    popd > /dev/null
+done
+
+echo
+echo OK!
+echo
