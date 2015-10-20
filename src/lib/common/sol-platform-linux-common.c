4945fada496f65d61c45ef26b760b404c1fa8c58
Leandro Dorileo <leandro.maciel.dorileo@intel.com>
2015-10-20 17:30:10 -0200

    linux-micro: uevent layer
    This patch introduces a common code to be used to handle uevent netlink
    notifications. This wrapps the event handling, set a sol_uevent struct
    with the collected data and calls a callback.
    
    This layer will be used by the automount and the kmod modules.
    
    Signed-off-by: Leandro Dorileo <leandro.maciel.dorileo@intel.com>

--- src/lib/common/sol-platform-linux-common.c
+++ src/lib/common/sol-platform-linux-common.c
@@ -32,11 +32,13 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <linux/netlink.h>
 #include <mntent.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mount.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/utsname.h>
@@ -53,6 +55,7 @@
 
 #define SOL_MTAB_FILE P_tmpdir "/mtab.sol"
 #define SYS_MTAB_FILE "/etc/mtab"
+#define LIBUDEV_ID "libudev"
 
 struct sol_platform_linux_fork_run {
     pid_t pid;
@@ -77,6 +80,23 @@ struct mount_data {
     void (*async_cb)(const char *dev, const char *mpoint, void *data, uint64_t pid, int status);
 };
 
+struct uevent_callback {
+    const char *action;
+    const char *subsystem;
+    const void *cb_data;
+    void (*uevent_cb)(void *cb_data, struct sol_uevent *uevent);
+};
+
+struct uevent_context {
+    bool running;
+    struct sol_vector callbacks;
+    struct {
+        int fd;
+        struct sol_fd *watch;
+    } uevent;
+};
+
+static struct uevent_context uevent_ctx;
 static struct sol_ptr_vector fork_runs = SOL_PTR_VECTOR_INIT;
 
 static uint16_t
@@ -698,4 +718,170 @@ sol_platform_impl_mount(const char *dev, const char *mpoint, const char *fstype,
 
     sol_platform_linux_fork_run(on_mount_fork, on_mount_fork_exit, mdata);
     return 0;
+}
+
+static void
+sol_uevent_event_dispatch(struct uevent_context *ctx, struct sol_uevent *uevent)
+{
+    uint16_t idx;
+    struct uevent_callback *cb;
+
+    SOL_VECTOR_FOREACH_IDX (&ctx->callbacks, cb, idx) {
+        if ((!cb->action || (cb->action && sol_str_slice_str_eq(uevent->action, cb->action))) &&
+            (!cb->subsystem || (cb->subsystem && sol_str_slice_str_eq(uevent->subsystem, cb->subsystem)))) {
+            cb->uevent_cb((void *)cb->cb_data, uevent);
+        }
+    }
+}
+
+static void
+sol_uevent_read_msg(struct uevent_context *ctx, char *msg, int len)
+{
+    struct sol_uevent uevent = {
+        .modalias = SOL_STR_SLICE_EMPTY,
+        .action = SOL_STR_SLICE_EMPTY,
+        .subsystem = SOL_STR_SLICE_EMPTY,
+        .devtype = SOL_STR_SLICE_EMPTY,
+        .devname = SOL_STR_SLICE_EMPTY,
+    };
+    int i = 0;
+
+    /** lets avoid it to misbehave when people trying to test it with systemd running */
+    if (!strncmp(msg, LIBUDEV_ID, sizeof(LIBUDEV_ID))) {
+        SOL_INF("We're running side-by-side with udevd, skipping udevd generated event");
+        return;
+    }
+
+    while (i < len) {
+        char *curr, *del;
+        size_t value_len;
+
+        curr = msg + i;
+        del = strrchr(curr, '=');
+        value_len = del - curr;
+
+        if (!strncmp(curr, "MODALIAS", value_len))
+            uevent.modalias = sol_str_slice_from_str(++del);
+        else if (!strncmp(curr, "ACTION", value_len))
+            uevent.action = sol_str_slice_from_str(++del);
+        else if (!strncmp(curr, "SUBSYSTEM", value_len))
+            uevent.subsystem = sol_str_slice_from_str(++del);
+        else if (!strncmp(curr, "DEVTYPE", value_len))
+            uevent.devtype = sol_str_slice_from_str(++del);
+        else if (!strncmp(curr, "DEVNAME", value_len))
+            uevent.devname = sol_str_slice_from_str(++del);
+
+        i += strlen(msg + i) + 1;
+    }
+
+    sol_uevent_event_dispatch(ctx, &uevent);
+}
+
+static bool
+sol_uevent_handler(void *data, int fd, unsigned int cond)
+{
+    int len;
+    struct uevent_context *ctx = data;
+    char buffer[512];
+
+    len = recv(ctx->uevent.fd, buffer, sizeof(buffer), MSG_WAITALL);
+    if (len == -1) {
+        SOL_ERR("Could not read netlink socket. %s", sol_util_strerrora(errno));
+        return false;
+    } else {
+        sol_uevent_read_msg(ctx, buffer, len);
+    }
+
+    return true;
+}
+
+static int
+sol_uevent_register(struct uevent_context *ctx)
+{
+    struct sockaddr_nl nls;
+
+    memset(&nls, 0, sizeof(struct sockaddr_nl));
+    nls.nl_family = AF_NETLINK;
+    nls.nl_pid = getpid();
+    nls.nl_groups = -1;
+
+    ctx->uevent.fd = socket(PF_NETLINK, SOCK_DGRAM | SOCK_CLOEXEC,
+        NETLINK_KOBJECT_UEVENT);
+    if (ctx->uevent.fd == -1) {
+        SOL_ERR("Could not open uevent netlink socket.");
+        return -errno;
+    }
+
+    if (bind(ctx->uevent.fd, (void *)&nls, sizeof(struct sockaddr_nl))) {
+        SOL_ERR("Could not bind to uevent socket.");
+        close(ctx->uevent.fd);
+        ctx->uevent.fd = 0;
+        return -errno;
+    }
+
+    ctx->uevent.watch = sol_fd_add(ctx->uevent.fd, SOL_FD_FLAGS_IN,
+        sol_uevent_handler, ctx);
+    return 0;
+}
+
+SOL_API int
+sol_platform_linux_uevent_subscribe(const char *action, const char *subsystem,
+    void (*uevent_cb)(void *cb_data, struct sol_uevent *uevent),
+    const void *cb_data)
+{
+    struct uevent_callback *cb;
+
+    if (!uevent_ctx.running)
+        sol_vector_init(&uevent_ctx.callbacks, sizeof(struct uevent_callback));
+
+    cb = sol_vector_append(&uevent_ctx.callbacks);
+    SOL_NULL_CHECK(cb, -1);
+
+    cb->action = action;
+    cb->subsystem = subsystem;
+    cb->uevent_cb = uevent_cb;
+    cb->cb_data = cb_data;
+
+    if (!uevent_ctx.running) {
+        sol_uevent_register(&uevent_ctx);
+    }
+
+    uevent_ctx.running = true;
+    return 0;
+}
+
+static void
+sol_uevent_cleaup(struct uevent_context *ctx)
+{
+    if (ctx->uevent.watch)
+        sol_fd_del(ctx->uevent.watch);
+
+    if (ctx->uevent.fd)
+        close(ctx->uevent.fd);
+
+    sol_vector_clear(&ctx->callbacks);
+    ctx->running = false;
+}
+
+SOL_API int
+sol_platform_linux_uevent_unsubscribe(const char *action, const char *subsystem,
+    void (*uevent_cb)(void *cb_data, struct sol_uevent *uevent))
+{
+    struct uevent_callback *callback;
+    uint16_t idx;
+    int res = -1;
+
+    SOL_NULL_CHECK(uevent_cb, -1);
+
+    SOL_VECTOR_FOREACH_REVERSE_IDX (&uevent_ctx.callbacks, callback, idx) {
+        if (callback->uevent_cb == uevent_cb) {
+            sol_vector_del(&uevent_ctx.callbacks, idx);
+            res = 0;
+        }
+    }
+
+    if (!uevent_ctx.callbacks.len)
+        sol_uevent_cleaup(&uevent_ctx);
+
+    return res;
 }
