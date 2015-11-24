70c46c9a821e0ce51b76b391476da03daac07816
Leandro Pereira <leandro.pereira@intel.com>
2015-11-24 14:34:09 -0200

    shared/random: URANDOM engine shouldn't allow being passed a seed
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>

--- src/shared/sol-random.c
+++ src/shared/sol-random.c
@@ -222,6 +222,11 @@ engine_urandom_init(struct sol_random *generic, uint64_t seed)
 {
     struct sol_random_urandom *engine = (struct sol_random_urandom *)generic;
 
+    if (seed) {
+        SOL_WRN("Explicit seed not supported by this random implementation");
+        return false;
+    }
+
     engine->fd = open("/dev/urandom", O_RDONLY | O_CLOEXEC);
     if (engine->fd < 0) {
         SOL_WRN("Could not open /dev/urandom: %s", sol_util_strerrora(errno));
