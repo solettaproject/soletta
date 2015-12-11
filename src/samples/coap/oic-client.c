68faebecaffc4a9635225348f4c24c1a98cf488c
Flavio Ceolin <flavio.ceolin@gmail.com>
2015-12-11 17:25:20 -0200

    oic:samples: Fix according with the new api
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- src/samples/coap/oic-client.c
+++ src/samples/coap/oic-client.c
@@ -40,11 +40,12 @@
 
 static void
 got_get_response(struct sol_oic_client *cli, const struct sol_network_link_addr *cliaddr,
-    const struct sol_str_slice *href, const struct sol_vector *repr, void *data)
+    const struct sol_str_slice *href, const struct sol_oic_map_reader *map_reader, void *data)
 {
-    struct sol_oic_repr_field *field;
+    struct sol_oic_repr_field field;
+    enum sol_oic_map_loop_reason end_reason;
+    struct sol_oic_map_reader iterator;
     char addr[SOL_INET_ADDR_STRLEN];
-    uint16_t idx;
 
     if (!sol_network_addr_to_str(cliaddr, addr, sizeof(addr))) {
         SOL_WRN("Could not convert network address to string");
@@ -52,39 +53,39 @@ got_get_response(struct sol_oic_client *cli, const struct sol_network_link_addr 
     }
 
     printf("Dumping payload received from addr %s {\n", addr);
-    SOL_VECTOR_FOREACH_IDX (repr, field, idx) {
-        printf("\tkey: '%s', value: ", field->key);
+    SOL_OIC_MAP_LOOP(map_reader, &field, &iterator, end_reason) {
+        printf("\tkey: '%s', value: ", field.key);
 
-        switch (field->type) {
+        switch (field.type) {
         case SOL_OIC_REPR_TYPE_UINT:
-            printf("uint(%" PRIu64 ")\n", field->v_uint);
+            printf("uint(%" PRIu64 ")\n", field.v_uint);
             break;
         case SOL_OIC_REPR_TYPE_INT:
-            printf("int(%" PRIi64 ")\n", field->v_int);
+            printf("int(%" PRIi64 ")\n", field.v_int);
             break;
         case SOL_OIC_REPR_TYPE_SIMPLE:
-            printf("simple(%d)\n", field->v_simple);
+            printf("simple(%d)\n", field.v_simple);
             break;
         case SOL_OIC_REPR_TYPE_TEXT_STRING:
-            printf("str(%.*s)\n", (int)field->v_slice.len, field->v_slice.data);
+            printf("str(%.*s)\n", (int)field.v_slice.len, field.v_slice.data);
             break;
         case SOL_OIC_REPR_TYPE_BYTE_STRING:
             printf("bytestr() [not dumping]\n");
             break;
         case SOL_OIC_REPR_TYPE_HALF_FLOAT:
-            printf("hfloat(%p)\n", field->v_voidptr);
+            printf("hfloat(%p)\n", field.v_voidptr);
             break;
         case SOL_OIC_REPR_TYPE_FLOAT:
-            printf("float(%f)\n", field->v_float);
+            printf("float(%f)\n", field.v_float);
             break;
         case SOL_OIC_REPR_TYPE_DOUBLE:
-            printf("float(%g)\n", field->v_double);
+            printf("float(%g)\n", field.v_double);
             break;
         case SOL_OIC_REPR_TYPE_BOOLEAN:
-            printf("boolean(%s)\n", field->v_boolean ? "true" : "false");
+            printf("boolean(%s)\n", field.v_boolean ? "true" : "false");
             break;
         default:
-            printf("unknown(%d)\n", field->type);
+            printf("unknown(%d)\n", field.type);
         }
     }
     printf("}\n\n");
