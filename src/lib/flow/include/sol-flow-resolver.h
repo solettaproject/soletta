9745228b16483e1f02b62a3070a8d17666ef86c5
Murilo Belluzzo <murilo.belluzzo@intel.com>
2015-12-18 20:09:14 -0200

    flow-resolver: Add Documentation
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- src/lib/flow/include/sol-flow-resolver.h
+++ src/lib/flow/include/sol-flow-resolver.h
@@ -46,49 +46,84 @@ extern "C" {
  */
 
 /**
+ * @defgroup FlowResolver Resolver
  * @ingroup Flow
+ *
+ * @brief A Resolver matches type names to an actual node_type and possibly a
+ * companion options.
+ *
+ * Typically, it would match type names directly, but other
+ * uses exist, e.g. a resolver could match IDs from a configuration
+ * file to concrete types.
+ *
+ * It's possible to have different Resolvers by using different heuristics
+ * to implement the @c revolve callback.
  *
  * @{
  */
 
 /**
- * @struct sol_flow_resolver
- *
- * Resolver matches type names to an actual node_type and possibly and
- * companion options.
- *
- * A resolver typically would match type names directly, but other
- * uses exist, e.g. a resolver could match IDs from a configuration
- * file to concrete types.
+ * @brief Resolver's structure.
  */
-
 struct sol_flow_resolver {
 #ifndef SOL_NO_API_VERSION
-#define SOL_FLOW_RESOLVER_API_VERSION (1UL)
-    unsigned long api_version;
+#define SOL_FLOW_RESOLVER_API_VERSION (1UL) /**< @brief Current API version number */
+    unsigned long api_version; /**< @brief API version number */
 #endif
-    const char *name;
-    void *data;
+    const char *name; /**< @brief Resolver's name (useful for logging) */
+    void *data; /**< @brief Resolver's context data (will be provided to @c resolve) */
 
+    /**
+     * @brief Resolution routine
+     *
+     * That is the routine used to resolve @a id into a node type.
+     *
+     * @param data Contextual data of this Resolver
+     * @param id Id that needs to be resolved
+     * @param type Where to return the node type of id
+     * @param named_opts Options of the found node type if available
+     *
+     * @return @c 0 in case of success, error code (always negative) otherwise
+     */
     int (*resolve)(void *data, const char *id, struct sol_flow_node_type const **type, struct sol_flow_node_named_options *named_opts);
 };
 
-/* Default resolver set at compile time. */
+/**
+ * @brief The default resolver set at compile time.
+ */
 const struct sol_flow_resolver *sol_flow_get_default_resolver(void);
 
-/* When resolver is NULL, use the default resolver.
+/**
+ * @brief Resolver for built-in node types.
  *
- * sol_flow_node_named_options_fini() must be used to delete members
- * in case of success. */
+ * A Resolver implementation that interprets IDs as node type names
+ * and return the appropriate built-in node type.
+ */
+const struct sol_flow_resolver *sol_flow_get_builtins_resolver(void);
+
+/**
+ * @brief Function used to resolve @a id into a node type.
+ *
+ * It uses @a resolver to translate @a id into a node type (returned in @a type)
+ * and its options if available after resolution.
+ *
+ * When resolver is @c NULL, the default resolver is used.
+ *
+ * @ref sol_flow_node_named_options_fini() must be used
+ * to delete members in case of success.
+ *
+ * @param resolver The resolver to be used to solve this id.
+ * @param id Id to be resolved
+ * @param type The node type for @a id if success, @c NULL otherwise
+ * @param named_opts Options for the resolved node type if available, @c NULL otherwise
+ *
+ * @return @c 0 in case of success, error code (always negative) otherwise
+ */
 int sol_flow_resolve(
     const struct sol_flow_resolver *resolver,
     const char *id,
     const struct sol_flow_node_type **type,
     struct sol_flow_node_named_options *named_opts);
-
-/* Resolver that interprets IDs as node type names and return the
- * appropriate builtin node type. */
-const struct sol_flow_resolver *sol_flow_get_builtins_resolver(void);
 
 /**
  * @}
