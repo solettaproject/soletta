6c8b19f6ec7759007e5f46995f951ce6edc0ebe7
Murilo Belluzzo <murilo.belluzzo@intel.com>
2015-12-21 16:56:24 -0200

    flow-parser: Add documentation
    Signed-off-by: Murilo Belluzzo <murilo.belluzzo@intel.com>

--- src/lib/flow/include/sol-flow-parser.h
+++ src/lib/flow/include/sol-flow-parser.h
@@ -47,7 +47,15 @@ extern "C" {
  */
 
 /**
+ * @defgroup FlowParser Flow Parser
  * @ingroup Flow
+ *
+ * @brief The Flow Parser is used to transform a textual description
+ * (either FBP or other metatypes) into node types that can be used in a flow.
+ *
+ * The node types created are owned by the parser object, so the
+ * parser can only be deleted after all its types are not used
+ * anymore.
  *
  * @{
  */
@@ -55,47 +63,104 @@ extern "C" {
 /**
  * @struct sol_flow_parser
  *
- * Parser is used to transform a flow description files into node
- * types that can be used in a flow.
- *
- * The node types created are owned by the parser object, so the
- * parser can only be deleted after all its types are not used
- * anymore. */
+ * @brief Flow Parser handle.
+ */
 struct sol_flow_parser;
 
+/**
+ * @brief Flow Parser's client structure.
+ */
 struct sol_flow_parser_client {
 #ifndef SOL_NO_API_VERSION
 #define SOL_FLOW_PARSER_CLIENT_API_VERSION (1UL)
-    unsigned long api_version;
+    unsigned long api_version; /**< @brief API version */
 #endif
-    void *data;
+    void *data; /**< @brief Client data */
 
-    /* Called by parser to load declared types, buf should be valid
-     * until the API call to parser returns. */
+    /**
+     * @brief Parser's read callback.
+     *
+     * Called by the parser to load declared types,
+     * @a buf should remain valid until the parser is finished.
+     *
+     * @param data Client's data
+     * @param name File name to read
+     * @param buf Where to store the file
+     * @param size Buffer's size
+     *
+     * @return @c 0 on success, error code (always negative) otherwise
+     */
     int (*read_file)(void *data, const char *name, const char **buf, size_t *size);
 };
 
-/* Resolver is used to find node types used by the nodes in the
- * flow. If not set, will use the default resolver. */
+/**
+ * @brief Creates a new instance of @ref sol_flow_parser.
+ *
+ * @param client Parser's client
+ * @param resolver @ref FlowResolver to be used
+ *
+ * @note Passing @c NULL to @a resolver will set the default resolver.
+ *
+ * @return A new Flow Parser instance, @c NULL on error.
+ */
 struct sol_flow_parser *sol_flow_parser_new(
     const struct sol_flow_parser_client *client,
     const struct sol_flow_resolver *resolver);
 
+/**
+ * @brief Destroy a @ref sol_flow_parser instance.
+ *
+ * @param parser Flow parser to be destroyed
+ *
+ * @return @c 0 on success, error code (always negative) otherwise
+ */
 int sol_flow_parser_del(
     struct sol_flow_parser *parser);
 
+/**
+ * @brief Parses a flow description contained in @a buf and returns the resulting node type.
+ *
+ * @param parser The Flow Parser handle
+ * @param buf Flow description to be parsed
+ * @param len Buffer's size
+ * @param filename Name of the file which content is in @a buf
+ *
+ * @return Resulting node type of the parsed FBP on success, @c NULL otherwise.
+ */
 struct sol_flow_node_type *sol_flow_parse_buffer(
     struct sol_flow_parser *parser,
     const char *buf,
     size_t len,
     const char *filename);
 
-/* Same as above but accepts C string. */
+/**
+ * @brief Similar to @ref sol_flow_parse_buffer but accepts C string.
+ *
+ * It will calculate the size of @a str and call @ref sol_flow_parse_buffer
+ * with the equivalent parameters.
+ *
+ * @param parser The Flow Parser handle
+ * @param str Flow description to be parsed
+ * @param filename Name of the file which content is in @a str
+ *
+ * @return Resulting node type of the parsed FBP on success, @c NULL otherwise.
+ */
 struct sol_flow_node_type *sol_flow_parse_string(
     struct sol_flow_parser *parser,
     const char *str,
     const char *filename);
 
+/**
+ * @brief Parsers a @a buf of a given @a metatype and returns the resulting node type.
+ *
+ * @param parser The Flow Parser handle
+ * @param metatype Content's type (e.g. "fbp", or "js")
+ * @param buf Content to be parsed
+ * @param len Buffer's size
+ * @param filename Name of the file which content is in @a buf
+ *
+ * @return Resulting node type of the parsed FBP on success, @c NULL otherwise.
+ */
 struct sol_flow_node_type *sol_flow_parse_buffer_metatype(
     struct sol_flow_parser *parser,
     const char *metatype,
@@ -103,7 +168,19 @@ struct sol_flow_node_type *sol_flow_parse_buffer_metatype(
     size_t len,
     const char *filename);
 
-/* Same as above but accepts C string. */
+/**
+ * @brief Similar to @ref sol_flow_parse_buffer_metatype but accepts C string.
+ *
+ * It will calculate the size of @a str and call @ref sol_flow_parse_buffer
+ * with the equivalent parameters.
+ *
+ * @param parser The Flow Parser handle
+ * @param metatype Content's type (e.g. "fbp", or "js")
+ * @param str Content to be parsed
+ * @param filename Name of the file which content is in @a str
+ *
+ * @return Resulting node type of the parsed FBP on success, @c NULL otherwise.
+ */
 struct sol_flow_node_type *sol_flow_parse_string_metatype(
     struct sol_flow_parser *parser,
     const char *metatype,
