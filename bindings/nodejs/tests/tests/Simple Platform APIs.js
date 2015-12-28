1b0274112796b063ab22a85c400556aca6229fab
Gabriel Schulhof <gabriel.schulhof@intel.com>
2015-12-28 13:27:16 +0200

    Node.js bindings: Add more simple platform bindings
    Closes gh-1476
    
    Signed-off-by: Gabriel Schulhof <gabriel.schulhof@intel.com>

---
+++ bindings/nodejs/tests/tests/Simple Platform APIs.js
@@ -0,0 +1,29 @@
+var path = require( "path" );
+var soletta = require( path.join( require( "../closestSoletta" )( __dirname ), "lowlevel" ) );
+var testUtils = require( "../assert-to-console" );
+var exec = require( "child_process" ).exec;
+var fs = require( "fs" );
+var childEnv = require( "lodash" ).extend( {}, process.env, { LD_PRELOAD: undefined } );
+
+console.log( JSON.stringify( { assertionCount: 3 } ) );
+
+// Use the UUID we were given as the soletta machine ID
+process.env.SOL_MACHINE_ID = process.argv[ 2 ].replace( /[-]/g, "" );
+testUtils.assert( "strictEqual", soletta.sol_platform_get_machine_id(),
+	process.env.SOL_MACHINE_ID, "Machine ID value is as expected" );
+
+exec( "hostname", { env: childEnv }, function( error, stdout ) {
+	if ( error ) {
+		testUtils.die( error );
+	}
+	testUtils.assert( "strictEqual", stdout.toString().trim(),
+		soletta.sol_platform_get_hostname(), "Hostname matches `hostname`" );
+} );
+
+exec( "uname -r", { env: childEnv }, function( error, stdout ) {
+	if ( error ) {
+		testUtils.die( error );
+	}
+	testUtils.assert( "strictEqual", stdout.toString().trim(),
+		soletta.sol_platform_get_os_version(), "OS version matches `uname -r`");
+} );
