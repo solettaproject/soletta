6c3dadb4dbbe886794545e384a77ea0a35a0124d
guchaojie <chao.jie.gu@intel.com>
2016-01-06 14:01:42 +0800

    flow:iio: Add Accelerate sensor Category support into iio node
    Change-Id: I62fe2cc4c5161046a88f2752c34f7bbc0785177f
    Signed-off-by: guchaojie <chao.jie.gu@intel.com>

--- src/modules/flow/iio/nodes.c
+++ src/modules/flow/iio/nodes.c
@@ -801,4 +801,158 @@ error:
     return -EIO;
 }
 
+struct accelerate_data {
+    struct sol_iio_config config;
+    struct sol_direction_vector scale;
+    struct sol_direction_vector offset;
+    struct sol_drange_spec out_range;
+    struct sol_iio_device *device;
+    struct sol_iio_channel *channel_x;
+    struct sol_iio_channel *channel_y;
+    struct sol_iio_channel *channel_z;
+    bool buffer_enabled : 1;
+    bool use_device_default_scale : 1;
+    bool use_device_default_offset : 1;
+};
+
+static void
+accelerate_reader_cb(void *data, struct sol_iio_device *device)
+{
+    static const char *errmsg = "Could not read channel buffer values";
+    struct sol_flow_node *node = data;
+    struct accelerate_data *mdata = sol_flow_node_get_private_data(node);
+    struct sol_direction_vector out = {
+        .min = mdata->out_range.min,
+        .max = mdata->out_range.max
+    };
+    bool b;
+
+    b = sol_iio_read_channel_value(mdata->channel_x, &out.x);
+    if (!b) goto error;
+
+    b = sol_iio_read_channel_value(mdata->channel_y, &out.y);
+    if (!b) goto error;
+
+    b = sol_iio_read_channel_value(mdata->channel_z, &out.z);
+    if (!b) goto error;
+
+    sol_flow_send_direction_vector_packet(node,
+        SOL_FLOW_NODE_TYPE_IIO_ACCELERATE__OUT__OUT, &out);
+
+    return;
+
+error:
+    sol_flow_send_error_packet_str(node, EIO, errmsg);
+    SOL_WRN("%s", errmsg);
+}
+
+static void
+accelerate_create_device_cb(void *data, int device_id)
+{
+    struct accelerate_data *mdata = data;
+    struct sol_iio_channel_config channel_config = SOL_IIO_CHANNEL_CONFIG_INIT;
+
+    mdata->device = sol_iio_open(device_id, &mdata->config);
+    SOL_NULL_CHECK(mdata->device);
+
+#define ADD_CHANNEL(_axis) \
+    if (!mdata->use_device_default_scale) \
+        channel_config.scale = mdata->scale._axis; \
+    if (!mdata->use_device_default_offset) \
+        channel_config.offset = mdata->offset._axis; \
+    mdata->channel_ ## _axis = sol_iio_add_channel(mdata->device, "in_accel_" # _axis, &channel_config); \
+    SOL_NULL_CHECK_GOTO(mdata->channel_ ## _axis, error);
+
+    ADD_CHANNEL(x);
+    ADD_CHANNEL(y);
+    ADD_CHANNEL(z);
+
+#undef ADD_CHANNEL
+
+    sol_iio_device_start_buffer(mdata->device);
+
+    return;
+
+error:
+    SOL_WRN("Could not create iio/accelerate node. Failed to open IIO device %d",
+        device_id);
+    sol_iio_close(mdata->device);
+}
+
+static int
+accelerate_open(struct sol_flow_node *node, void *data, const struct sol_flow_node_options *options)
+{
+    struct accelerate_data *mdata = data;
+    const struct sol_flow_node_type_iio_accelerate_options *opts;
+
+    SOL_FLOW_NODE_OPTIONS_SUB_API_CHECK(options, SOL_FLOW_NODE_TYPE_IIO_ACCELERATE_OPTIONS_API_VERSION,
+        -EINVAL);
+    opts = (const struct sol_flow_node_type_iio_accelerate_options *)options;
+
+    mdata->buffer_enabled = opts->buffer_size > -1;
+
+    SOL_SET_API_VERSION(mdata->config.api_version = SOL_IIO_CONFIG_API_VERSION; )
+
+    if (opts->iio_trigger_name) {
+        mdata->config.trigger_name = strdup(opts->iio_trigger_name);
+        SOL_NULL_CHECK(mdata->config.trigger_name, -ENOMEM);
+    }
+
+    mdata->config.buffer_size = opts->buffer_size;
+    mdata->config.sampling_frequency = opts->sampling_frequency;
+    if (mdata->buffer_enabled) {
+        mdata->config.sol_iio_reader_cb = accelerate_reader_cb;
+        mdata->config.data = node;
+    }
+    mdata->use_device_default_scale = opts->use_device_default_scale;
+    mdata->use_device_default_offset = opts->use_device_default_offset;
+    mdata->scale = opts->scale;
+    mdata->offset = opts->offset;
+    mdata->out_range = opts->out_range;
+
+    if (!sol_iio_address_device(opts->iio_device, accelerate_create_device_cb, mdata)) {
+        SOL_WRN("Could not create iio/accelerate node. Failed to open IIO device %s",
+            opts->iio_device);
+        goto err;
+    }
+
+    return 0;
+
+err:
+    free((char *)mdata->config.trigger_name);
+    return -EINVAL;
+}
+
+static void
+accelerate_close(struct sol_flow_node *node, void *data)
+{
+    struct accelerate_data *mdata = data;
+
+    free((char *)mdata->config.trigger_name);
+    if (mdata->device)
+        sol_iio_close(mdata->device);
+}
+
+static int
+accelerate_tick(struct sol_flow_node *node, void *data, uint16_t port, uint16_t conn_id, const struct sol_flow_packet *packet)
+{
+    static const char *errmsg = "Could not read channel values";
+    struct accelerate_data *mdata = data;
+
+    if (mdata->buffer_enabled) {
+        if (!sol_iio_device_trigger_now(mdata->device))
+            goto error;
+    } else {
+        accelerate_reader_cb(mdata, mdata->device);
+    }
+
+    return 0;
+
+error:
+    sol_flow_send_error_packet(node, EIO, "%s", errmsg);
+    SOL_WRN("%s", errmsg);
+
+    return -EIO;
+}
+
 #include "iio-gen.c"
