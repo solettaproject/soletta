eb21feea69f3de5ba9f6a31d4ab6cb40bbf539c6
guchaojie <chao.jie.gu@intel.com>
2016-01-06 11:12:47 +0800

    flow:iio: Add Color sensor Category support into iio node
    Change-Id: Ia2538ac8cce9153243326c69a20f5c82288e1ea9
    Signed-off-by: guchaojie <chao.jie.gu@intel.com>

--- src/modules/flow/iio/nodes.c
+++ src/modules/flow/iio/nodes.c
@@ -642,4 +642,163 @@ error:
     return -EIO;
 }
 
+struct color_data {
+    struct sol_iio_config config;
+    double scale;
+    double offset;
+    struct sol_drange_spec out_range;
+    struct sol_iio_device *device;
+    struct sol_iio_channel *channel_red;
+    struct sol_iio_channel *channel_green;
+    struct sol_iio_channel *channel_blue;
+    bool buffer_enabled : 1;
+    bool use_device_default_scale : 1;
+    bool use_device_default_offset : 1;
+};
+
+static void
+color_reader_cb(void *data, struct sol_iio_device *device)
+{
+    static const char *errmsg = "Could not read channel buffer values";
+    struct sol_flow_node *node = data;
+    struct color_data *mdata = sol_flow_node_get_private_data(node);
+    struct sol_rgb out = {
+        .red_max = mdata->out_range.max,
+        .green_max = mdata->out_range.max,
+        .blue_max = mdata->out_range.max
+    };
+    double tmp;
+    bool b;
+
+    b = sol_iio_read_channel_value(mdata->channel_red, &tmp);
+    if (!b || tmp < 0 || tmp > UINT32_MAX) goto error;
+    out.red = tmp;
+
+    b = sol_iio_read_channel_value(mdata->channel_green, &tmp);
+    if (!b || tmp < 0 || tmp > UINT32_MAX) goto error;
+    out.green = tmp;
+
+    b = sol_iio_read_channel_value(mdata->channel_blue, &tmp);
+    if (!b || tmp < 0 || tmp > UINT32_MAX) goto error;
+    out.blue = tmp;
+
+    sol_flow_send_rgb_packet(node,
+        SOL_FLOW_NODE_TYPE_IIO_COLOR__OUT__OUT, &out);
+
+    return;
+
+error:
+    sol_flow_send_error_packet_str(node, EIO, errmsg);
+    SOL_WRN("%s", errmsg);
+}
+
+static void
+color_create_device_cb(void *data, int device_id)
+{
+    struct color_data *mdata = data;
+    struct sol_iio_channel_config channel_config = SOL_IIO_CHANNEL_CONFIG_INIT;
+
+    mdata->device = sol_iio_open(device_id, &mdata->config);
+    SOL_NULL_CHECK(mdata->device);
+
+#define ADD_CHANNEL(_axis) \
+    if (!mdata->use_device_default_scale) \
+        channel_config.scale = mdata->scale; \
+    if (!mdata->use_device_default_offset) \
+        channel_config.offset = mdata->offset; \
+    mdata->channel_ ## _axis = sol_iio_add_channel(mdata->device, "in_intensity_" # _axis, &channel_config); \
+    SOL_NULL_CHECK_GOTO(mdata->channel_ ## _axis, error);
+
+    ADD_CHANNEL(red);
+    ADD_CHANNEL(green);
+    ADD_CHANNEL(blue);
+
+#undef ADD_CHANNEL
+
+    sol_iio_device_start_buffer(mdata->device);
+
+    return;
+
+error:
+    SOL_WRN("Could not create iio/color node. Failed to open IIO device %d",
+        device_id);
+    sol_iio_close(mdata->device);
+}
+
+static int
+color_open(struct sol_flow_node *node, void *data, const struct sol_flow_node_options *options)
+{
+    struct color_data *mdata = data;
+    const struct sol_flow_node_type_iio_color_options *opts;
+
+    SOL_FLOW_NODE_OPTIONS_SUB_API_CHECK(options, SOL_FLOW_NODE_TYPE_IIO_COLOR_OPTIONS_API_VERSION,
+        -EINVAL);
+    opts = (const struct sol_flow_node_type_iio_color_options *)options;
+
+    mdata->buffer_enabled = opts->buffer_size > -1;
+
+    SOL_SET_API_VERSION(mdata->config.api_version = SOL_IIO_CONFIG_API_VERSION; )
+
+    if (opts->iio_trigger_name) {
+        mdata->config.trigger_name = strdup(opts->iio_trigger_name);
+        SOL_NULL_CHECK(mdata->config.trigger_name, -ENOMEM);
+    }
+
+    mdata->config.buffer_size = opts->buffer_size;
+    mdata->config.sampling_frequency = opts->sampling_frequency;
+    if (mdata->buffer_enabled) {
+        mdata->config.sol_iio_reader_cb = color_reader_cb;
+        mdata->config.data = node;
+    }
+    mdata->use_device_default_scale = opts->use_device_default_scale;
+    mdata->use_device_default_offset = opts->use_device_default_offset;
+    mdata->scale = opts->scale;
+    mdata->offset = opts->offset;
+    mdata->out_range = opts->out_range;
+
+    if (!sol_iio_address_device(opts->iio_device, color_create_device_cb, mdata)) {
+        SOL_WRN("Could not create iio/color node. Failed to open IIO device %s",
+            opts->iio_device);
+        goto err;
+    }
+
+    return 0;
+
+err:
+    free((char *)mdata->config.trigger_name);
+    return -EINVAL;
+}
+
+static void
+color_close(struct sol_flow_node *node, void *data)
+{
+    struct color_data *mdata = data;
+
+    free((char *)mdata->config.trigger_name);
+    if (mdata->device)
+        sol_iio_close(mdata->device);
+}
+
+static int
+color_tick(struct sol_flow_node *node, void *data, uint16_t port, uint16_t conn_id, const struct sol_flow_packet *packet)
+{
+    static const char *errmsg = "Could not read channel values";
+    struct color_data *mdata = data;
+
+    if (mdata->buffer_enabled) {
+        if (!sol_iio_device_trigger_now(mdata->device))
+            goto error;
+    } else {
+        color_reader_cb(mdata, mdata->device);
+    }
+
+    return 0;
+
+error:
+    sol_flow_send_error_packet(node, EIO, "%s", errmsg);
+    SOL_WRN("%s", errmsg);
+
+    return -EIO;
+}
+
 #include "iio-gen.c"
