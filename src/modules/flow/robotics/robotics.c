7afa54e04c85ad741176deb47ccc4cb69751d5bb
Leandro Pereira <leandro.pereira@intel.com>
2016-01-12 13:31:10 -0200

    flow: Add robotics module
    This commit adds the robotics module, implementing a simple H-Bridge
    node type. Usage is quite simple:
    
    	steering OUT -> IN leftbridge(hbridge)
    	emergency_brake OUT -> BRAKE leftbridge
    	leftbridge OUT[0] -> IN _(gpio/writer)
    	leftbridge OUT[1] -> IN _(gpio/writer)
    	leftbridge OUT[2] -> IN _(gpio/writer)
    	leftbridge OUT[3] -> IN _(gpio/writer)
    	leftbridge THROTTLE -> DUTY_CYCLE_PERENT _(pwm)
    
    The `steering` node will calculate an irange IN value that:
    
    	* If 0, will coast the motor
    	* If > 0, will turn the motor clockwise
    	* If < 0, will turn the motor counter-clockwise
    
    The THROTTLE output is an integer value in the 0-100 range, and is
    calculated by mapping the IN value's value, minimum value, and maximum
    value to the desired range.  This is appropriate to connect to a PWM
    node's DUTY_CYCLE_PERCENT input port.
    
    The steering node could be, for instance, a skid steering node capable
    of obtaining information from quadrature encoders and turn angle and
    control two H-Bridges to turn a tank-like robot.
    
    Closes #1261.
    
    Signed-off-by: Leandro Pereira <leandro.pereira@intel.com>

---
+++ src/modules/flow/robotics/robotics.c
@@ -0,0 +1,145 @@
+/*
+ * This file is part of the Soletta Project
+ *
+ * Copyright (C) 2016 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <errno.h>
+
+#include "sol-flow-internal.h"
+#include "sol-flow/robotics.h"
+#include "sol-log-internal.h"
+
+enum switches {
+    SW_ALL_OFF = 0,
+    SW_S1 = 1 << 0,
+        SW_S2 = 1 << 1,
+        SW_S3 = 1 << 2,
+        SW_S4 = 1 << 3
+};
+
+struct hbridge_data {
+    bool inverted;
+};
+
+static int
+hbridge_command(struct sol_flow_node *node, enum switches switches)
+{
+    int r;
+
+    r = sol_flow_send_boolean_packet(node,
+        SOL_FLOW_NODE_TYPE_ROBOTICS_HBRIDGE_L293D__OUT__OUT_0,
+        switches & SW_S1);
+    SOL_INT_CHECK(r, < 0, r);
+
+    r = sol_flow_send_boolean_packet(node,
+        SOL_FLOW_NODE_TYPE_ROBOTICS_HBRIDGE_L293D__OUT__OUT_0 + 1,
+        switches & SW_S2);
+    SOL_INT_CHECK(r, < 0, r);
+
+    r = sol_flow_send_boolean_packet(node,
+        SOL_FLOW_NODE_TYPE_ROBOTICS_HBRIDGE_L293D__OUT__OUT_0 + 2,
+        switches & SW_S3);
+    SOL_INT_CHECK(r, < 0, r);
+
+    r = sol_flow_send_boolean_packet(node,
+        SOL_FLOW_NODE_TYPE_ROBOTICS_HBRIDGE_L293D__OUT__OUT_0 + 3,
+        switches & SW_S4);
+    SOL_INT_CHECK(r, < 0, r);
+
+    return 0;
+}
+
+static int64_t
+map_to_percentage(int64_t in_value, int64_t in_min, int64_t in_max)
+{
+    if (SOL_UNLIKELY(in_max == in_min))
+        return 0;
+
+    return ((in_value - in_min) * 100) / (in_max - in_min);
+}
+
+static int
+hbridge_process_in(struct sol_flow_node *node, void *data,
+    uint16_t port, uint16_t conn_id, const struct sol_flow_packet *packet)
+{
+    struct hbridge_data *priv = data;
+    struct sol_irange value;
+    int r;
+
+    r = sol_flow_packet_get_irange(packet, &value);
+    SOL_INT_CHECK(r, < 0, r);
+
+    if (value.val == 0) {
+        r = hbridge_command(node, SW_ALL_OFF);
+    } else {
+        if (priv->inverted)
+            value.val = -value.val;
+
+        if (value.val > 0) {
+            r = hbridge_command(node, SW_S1 | SW_S4);
+        } else {
+            value.val = -value.val;
+            r = hbridge_command(node, SW_S2 | SW_S3);
+        }
+    }
+
+    if (!r) {
+        int64_t pct = map_to_percentage(value.val, value.min, value.max);
+
+        r = sol_flow_send_irange_value_packet(node,
+            SOL_FLOW_NODE_TYPE_ROBOTICS_HBRIDGE_L293D__OUT__THROTTLE,
+            pct);
+    }
+
+    return r;
+}
+
+static int
+hbridge_process_brake(struct sol_flow_node *node, void *data,
+    uint16_t port, uint16_t conn_id, const struct sol_flow_packet *packet)
+{
+    return hbridge_command(node, SW_S1 | SW_S3);
+}
+
+static int
+hbridge_open(struct sol_flow_node *node, void *data,
+    const struct sol_flow_node_options *options)
+{
+    struct hbridge_data *priv = data;
+    const struct sol_flow_node_type_robotics_hbridge_l293d_options *opts =
+        (const struct sol_flow_node_type_robotics_hbridge_l293d_options *)options;
+
+    priv->inverted = opts->inverted;
+
+    return 0;
+}
+
+#include "robotics-gen.c"
