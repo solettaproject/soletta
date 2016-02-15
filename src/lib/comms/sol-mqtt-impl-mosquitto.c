1d55a456ca14880213b1304d86efff1e81118f67
Guilherme Iscaro de Godoy <guilherme.iscaro@intel.com>
2016-02-15 11:30:12 -0200

    Fix invalid terminator at mqtt implementation.
    Signed-off-by: Guilherme Iscaro <guilherme.iscaro@intel.com>

--- src/lib/comms/sol-mqtt-impl-mosquitto.c
+++ src/lib/comms/sol-mqtt-impl-mosquitto.c
@@ -99,7 +99,7 @@ sol_mqtt_message_new(const char *topic, const struct sol_buffer *payload, sol_mq
     message = calloc(1, sizeof(struct sol_mqtt_message));
     SOL_NULL_CHECK(message, NULL);
 
-    SOL_SET_API_VERSION(message->api_version = SOL_MQTT_MESSAGE_API_VERSION, )
+    SOL_SET_API_VERSION(message->api_version = SOL_MQTT_MESSAGE_API_VERSION; )
 
     message->topic = sol_util_memdup(topic, strlen(topic) + 1);
     SOL_NULL_CHECK_GOTO(message->topic, topic_error);
