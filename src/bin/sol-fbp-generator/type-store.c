7722ee5cdbc4baf7e6e26020efa9f45851a3644c
Iván Briano <ivan.briano@intel.com>
2016-02-16 18:57:57 -0200

    flow: Don't assume node-type headers are all in the same place
    The FBP generator assumes that all node types headers can be found
    under sol-flow/ in the include directory. This is wrong, as the
    node-type generator may take a 'namespace' parameter where the headers
    will be stored.
    
    In order for the generator to do the right thing, store the namespace +
    header name in the JSON description generated, so the FBP generator can
    make use of it to include the right file.
    
    Signed-off-by: Iván Briano <ivan.briano@intel.com>

--- src/bin/sol-fbp-generator/type-store.c
+++ src/bin/sol-fbp-generator/type-store.c
@@ -59,6 +59,7 @@ struct type_store {
 CONST_SLICE(NAME_SLICE, "name");
 CONST_SLICE(SYMBOL_SLICE, "symbol");
 CONST_SLICE(OPTIONS_SYMBOL_SLICE, "options_symbol");
+CONST_SLICE(HEADER_FILE, "header_file");
 CONST_SLICE(IN_PORTS_SLICE, "in_ports");
 CONST_SLICE(OUT_PORTS_SLICE, "out_ports");
 CONST_SLICE(DATA_TYPE_SLICE, "data_type");
@@ -215,6 +216,7 @@ type_description_init(struct type_description *desc)
     desc->name = NULL;
     desc->symbol = NULL;
     desc->options_symbol = NULL;
+    desc->header_file = NULL;
     sol_vector_init(&desc->in_ports, sizeof(struct port_description));
     sol_vector_init(&desc->out_ports, sizeof(struct port_description));
     sol_vector_init(&desc->options, sizeof(struct option_description));
@@ -291,6 +293,7 @@ type_description_fini(struct type_description *desc)
     free(desc->name);
     free(desc->symbol);
     free(desc->options_symbol);
+    free(desc->header_file);
 }
 
 static bool
@@ -742,6 +745,11 @@ read_type(struct decoder *d, struct type_description *desc)
             if (!read_string_property_value(d, &value))
                 return false;
             desc->options_symbol = get_string(&value);
+
+        } else if (sol_str_slice_eq(key_slice, HEADER_FILE)) {
+            if (!read_string_property_value(d, &value))
+                return false;
+            desc->header_file = get_string(&value);
 
         } else if (sol_str_slice_eq(key_slice, IN_PORTS_SLICE)) {
             if (!read_ports_array(d, &desc->in_ports))
