00689479041aef0308ca8b2abb2ace2e0a02788a
Guilherme Iscaro de Godoy <guilherme.iscaro@intel.com>
2016-02-18 11:51:52 -0200

    Compiler warnings: Fix Incorrect format.
    Signed-off-by: Guilherme Iscaro <guilherme.iscaro@intel.com>

--- src/shared/sol-fbp-parser.c
+++ src/shared/sol-fbp-parser.c
@@ -244,7 +244,7 @@ handle_meta_error(struct sol_fbp_parser *p, int node, struct sol_str_slice *key,
         }
     }
 
-    return set_parse_error(p, "Couldn't add option '%.s*': %s", (int)key->len, key->data, sol_util_strerrora(error));
+    return set_parse_error(p, "Couldn't add option '%.*s': %s", (int)key->len, key->data, sol_util_strerrora(error));
 }
 
 static bool
