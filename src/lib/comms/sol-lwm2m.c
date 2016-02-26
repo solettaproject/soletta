bce955935be85f73e28d771430bcd333233f7271
Guilherme Iscaro de Godoy <guilherme.iscaro@intel.com>
2016-02-26 16:24:28 -0300

    Sol-lwm2m: Do not send CoAP notifications to objects/instances that have already been notified.
    Signed-off-by: Guilherme Iscaro <guilherme.iscaro@intel.com>

--- src/lib/comms/sol-lwm2m.c
+++ src/lib/comms/sol-lwm2m.c
@@ -3832,16 +3832,32 @@ find_resource_ctx_by_id(struct obj_instance *instance, uint16_t id)
     return NULL;
 }
 
+static bool
+notification_already_sent(struct sol_ptr_vector *vector, const void *ptr)
+{
+    uint16_t i;
+    const void *v;
+
+    SOL_PTR_VECTOR_FOREACH_IDX (vector, v, i) {
+        if (v == ptr)
+            return true;
+    }
+
+    return false;
+}
+
 SOL_API int
 sol_lwm2m_notify_observers(struct sol_lwm2m_client *client, const char **paths)
 {
     size_t i;
+    struct sol_ptr_vector already_sent = SOL_PTR_VECTOR_INIT;
+    int r;
 
     SOL_NULL_CHECK(client, -EINVAL);
     SOL_NULL_CHECK(paths, -EINVAL);
 
     for (i = 0; paths[i]; i++) {
-        bool r;
+        bool r_bool;
         uint16_t j, k;
         struct obj_ctx *obj_ctx;
         struct obj_instance *obj_instance;
@@ -3855,7 +3871,7 @@ sol_lwm2m_notify_observers(struct sol_lwm2m_client *client, const char **paths)
         if (tokens.len != 4) {
             sol_vector_clear(&tokens);
             SOL_WRN("The path must contain an object, instance id and resource id");
-            return -EINVAL;
+            goto err_exit;
         }
 
         k = 0;
@@ -3864,33 +3880,53 @@ sol_lwm2m_notify_observers(struct sol_lwm2m_client *client, const char **paths)
             if (j == 0)
                 continue;
             path[k++] = sol_util_strtoul(token->data, &end, token->len, 10);
-            r = errno;
             if (end == token->data || end != token->data + token->len ||
                 errno != 0) {
                 r = errno;
                 SOL_WRN("Could not convert %.*s to integer",
                     SOL_STR_SLICE_PRINT(*token));
                 sol_vector_clear(&tokens);
-                return r;
+                goto err_exit;
             }
         }
+
         sol_vector_clear(&tokens);
 
         obj_ctx = find_object_ctx_by_id(client, path[0]);
-        SOL_NULL_CHECK(obj_ctx, -EINVAL);
+        SOL_NULL_CHECK_GOTO(obj_ctx, err_exit_einval);
         obj_instance = find_object_instance_by_instance_id(obj_ctx, path[1]);
-        SOL_NULL_CHECK(obj_instance, -EINVAL);
+        SOL_NULL_CHECK_GOTO(obj_instance, err_exit_einval);
         res_ctx = find_resource_ctx_by_id(obj_instance, path[2]);
-        SOL_NULL_CHECK(res_ctx, -EINVAL);
+        SOL_NULL_CHECK_GOTO(res_ctx, err_exit_einval);
 
-        r = send_notification_pkt(client, obj_ctx, NULL, -1, obj_ctx->obj_res);
-        SOL_EXP_CHECK(!r, -EINVAL);
-        r = send_notification_pkt(client, obj_ctx, obj_instance, -1,
-            obj_instance->instance_res);
-        SOL_EXP_CHECK(!r, -EINVAL);
-        r = send_notification_pkt(client, obj_ctx, obj_instance, path[2],
+        if (!notification_already_sent(&already_sent, obj_ctx)) {
+            r_bool = send_notification_pkt(client, obj_ctx, NULL, -1, obj_ctx->obj_res);
+            SOL_EXP_CHECK_GOTO(!r_bool, err_exit_einval);
+
+            r = sol_ptr_vector_append(&already_sent, obj_ctx);
+            SOL_INT_CHECK_GOTO(r, < 0, err_exit);
+        }
+
+        if (!notification_already_sent(&already_sent, obj_instance)) {
+            r_bool = send_notification_pkt(client, obj_ctx, obj_instance, -1,
+                obj_instance->instance_res);
+            SOL_EXP_CHECK_GOTO(!r_bool, err_exit_einval);
+
+            r = sol_ptr_vector_append(&already_sent, obj_instance);
+            SOL_INT_CHECK_GOTO(r, < 0, err_exit);
+        }
+
+        r_bool = send_notification_pkt(client, obj_ctx, obj_instance, path[2],
             res_ctx->res);
-        SOL_EXP_CHECK(!r, -EINVAL);
+        SOL_EXP_CHECK_GOTO(!r_bool, err_exit_einval);
     }
+
+    sol_ptr_vector_clear(&already_sent);
     return 0;
+
+err_exit_einval:
+    r = -EINVAL;
+err_exit:
+    sol_ptr_vector_clear(&already_sent);
+    return r;
 }
