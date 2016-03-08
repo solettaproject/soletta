73faaa08dce31d346fe38164e53e8f307c3e7a4a
Gabriel Schulhof <gabriel.schulhof@intel.com>
2016-03-08 02:32:10 +0200

    Node.js bindings: Place awk portion of generate script into its own file
    Signed-off-by: Gabriel Schulhof <gabriel.schulhof@intel.com>

--- bindings/nodejs/generate-main.sh
+++ bindings/nodejs/generate-main.sh
@@ -40,66 +40,7 @@ for file in $FILES; do
 	echo "#include <$file>" >> bindings/nodejs/generated/main.h
 	for path in $SOLETTA_SEARCH_PATHS; do
 		if test -f $path/$file; then
-			cat $path/$file | awk '
-				BEGIN {
-					enum_values[0] = 0;
-					delete enum_values[0];
-					enum_name = "";
-					inside_enum = 0;
-					new_enum = 0;
-					last_line_was_blank=0;
-				}
-				/^#define/ {
-					if ( NF > 2 && $2 ~ /^[A-Za-z_][_A-Za-z0-9]*$/ ) {
-						print "  SET_CONSTANT_" ( ( substr($3, 1, 1) == "\"" ) ? "STRING": "NUMBER" ) "(target, " $2 ");" >> "bindings/nodejs/generated/main.cc"
-						last_line_was_blank = 0;
-					}
-				}
-				/^(typedef\s+)?enum\s+[^{]*{$/ {
-					enum_name = ( $2 == "enum" ) ? $3 : $2;
-					gsub(/{/, "", enum_name);
-					inside_enum = 1;
-					new_enum = 1;
-				}
-				/\s*}\s*(\S*)?\s*;\s*$/ {
-					if ( inside_enum == 1 ) {
-						if ( enum_name == "" ) {
-							enum_name = $0;
-							gsub(/(\s|[};])/, "", enum_name);
-						}
-						if ( enum_name != "" ) {
-							if ( last_line_was_blank == 0 ) {
-								print "" >> "bindings/nodejs/generated/main.cc"
-							}
-							print "  Local<Object> bind_" enum_name " = Nan::New<Object>();" >> "bindings/nodejs/generated/main.cc"
-							for ( enum_value in enum_values ) {
-								print "  SET_CONSTANT_NUMBER(bind_" enum_name ", " enum_value ");" >> "bindings/nodejs/generated/main.cc"
-							}
-							for ( enum_value in enum_values ) {
-								delete enum_values[ enum_value ];
-							}
-							print "  SET_CONSTANT_OBJECT(target, " enum_name ");" >> "bindings/nodejs/generated/main.cc"
-							print "" >> "bindings/nodejs/generated/main.cc"
-							last_line_was_blank = 1;
-						}
-						enum_name = "";
-						inside_enum = 0;
-					}
-				}
-				{
-					if ( new_enum == 1 ) {
-						new_enum = 0;
-					}
-					else
-					if ( inside_enum == 1 ) {
-						enum_member = $1;
-						gsub( /,/, "", enum_member );
-						if ( enum_member ~ /[A-Za-z][A-Za-z0-9]*/ ) {
-							enum_values[ enum_member ] = 0;
-						}
-					}
-				}
-			'
+			cat $path/$file | awk -f bindings/nodejs/generate-main.awk
 		fi
 	done
 done
