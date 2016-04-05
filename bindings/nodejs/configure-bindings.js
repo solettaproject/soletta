8d43e5fc53282636744f9f18ec246a89ec447ded
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2016-04-05 19:32:07 +0300

    Node.js bindings: Add JavaScript bindings for PWM
    This provides native and highlevel JavaScript bindings for PWM.
    
    Signed-off-by: Sudarsana Nagineni <sudarsana.nagineni@intel.com>
    Signed-off-by: Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>

--- bindings/nodejs/configure-bindings.js
+++ bindings/nodejs/configure-bindings.js
@@ -80,6 +80,15 @@ for ( oneVariable in process.env ) {
 				"sol-gpio.h"
 			] );
 			break;
+		case "USE_PWM":
+			sources = sources.concat( [
+				"../src/functions/pwm.cc",
+				"../src/structures/sol-js-pwm.cc"
+			] );
+			headers = headers.concat( [
+				"sol-pwm.h"
+			] );
+			break;
 		default:
 			break;
 	}
