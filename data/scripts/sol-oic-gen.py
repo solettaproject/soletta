c1f597cf9170f16b133df536ee5b0e6a391f42b0
Bruno Dilly <bruno.dilly@intel.com>
2016-04-05 19:53:42 -0300

    oic-gen: Properly handle camelCase property names
    Closes gh-1790
    
    Signed-off-by: Bruno Dilly <bruno.dilly@intel.com>

--- data/scripts/sol-oic-gen.py
+++ data/scripts/sol-oic-gen.py
@@ -20,6 +20,7 @@ import os
 import json
 import sys
 import traceback
+import re
 from collections import OrderedDict
 
 def merge_schema(directory, definitions, to_merge):
@@ -488,7 +489,7 @@ def object_inform_flow_fn_common_c(state_struct_name, name, props, client):
         send_flow_pkts.append('''%(flow_send_fn)s(resource->node, SOL_FLOW_NODE_TYPE_%(STRUCT_NAME)s__OUT__%(FIELD_NAME)s, %(val)s);''' % {
             'flow_send_fn': fn,
             'STRUCT_NAME': name.upper(),
-            'FIELD_NAME': field_name.upper(),
+            'FIELD_NAME': get_port_name(field_name),
             'val': val
         })
 
@@ -551,7 +552,7 @@ def object_open_fn_client_c(state_struct_name, resource_type, name, props):
 }
 ''' % {
         'struct_name': name,
-        'STRUCT_NAME': name.upper(),
+        'STRUCT_NAME': get_port_name(name),
         'resource_type': resource_type,
         'field_init': '\n'.join(field_init),
         'to_repr_vec_fn': to_repr_vec_fn
@@ -826,6 +827,10 @@ struct %(struct_name)s {
         'from_repr_vec_fn': object_from_repr_vec_fn_common_c(name, props),
     }
 
+# handle port_name, portName and PortName
+def get_port_name(name):
+    return re.sub('(?!^)([A-Z]+)', r'_\1', name).upper()
+
 def generate_object_json(resource_type, struct_name, node_name, title, props, server):
     if server:
         in_ports = []
@@ -860,7 +865,7 @@ def generate_object_json(resource_type, struct_name, node_name, title, props, se
             'methods': {
                 'process': '%s_set_%s' % (struct_name, prop_name)
             },
-            'name': '%s' % prop_name.upper()
+            'name': '%s' % get_port_name(prop_name)
         })
 
     if server:
@@ -880,7 +885,7 @@ def generate_object_json(resource_type, struct_name, node_name, title, props, se
         out_ports.append({
             'data_type': JSON_TO_SOL_JSON[prop_descr.get('type', 'string')],
             'description': prop_descr.get('description', '???'),
-            'name': '%s' % prop_name.upper()
+            'name': '%s' % get_port_name(prop_name)
         })
 
     output = {
