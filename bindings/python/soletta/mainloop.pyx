30398c39a799840accbc69cb73809039129fb24f
Kevron Rees <kevron.m.rees@intel.com>
2016-04-06 16:31:14 -0700

    mainloop implementation using asyncio

--- bindings/python/soletta/mainloop.pyx
+++ bindings/python/soletta/mainloop.pyx
@@ -1,11 +1,16 @@
 import asyncio
-
+from libc.stdint cimport uint32_t
 
 cdef extern from "sol-mainloop.h":
 	struct sol_mainloop_implementation:
 		void run()
-		bint idle_add()
-		void idle_del()
+		void init()
+		void quit()
+		void * idle_add(bint (*)(void*), void*)
+		bint idle_del(void*)
+		void* timeout_add(uint32_t timeout_ms, bint (*cb)(void *), const void *)
+		bint timeout_del(void*)
+		void shutdown()
 		pass
 
 	bint sol_mainloop_set_implementation(const sol_mainloop_implementation *)
@@ -23,20 +28,39 @@ cdef void wrap_sol_quit():
 cdef void wrap_sol_run():
 	asyncio.get_event_loop().run_forever()
 
-cdef void *wrap_sol_timeout_add(args):
-	pass
+cdef void *wrap_sol_timeout_add(uint32_t timeout_ms, bint (*cb)(void *), const void *data):
+	
+	@asyncio.coroutine
+	def task():
+		while cb(data):
+			yield from asyncio.sleep(timeout_ms / 1000)
 
-cdef void wrap_sol_timeout_del(args):
-	pass
+	t = asyncio.get_event_loop().create_task(task())
+	return <void*> t
 
-cdef bint wrap_idle_add(args):
-	return False
+cdef bint wrap_sol_timeout_del(void *handle):
+	(<object?>handle).cancel()
+	return True
 
-cdef void wrap_idle_del(args):
-	pass
+cdef void *wrap_idle_add(bint (*cb)(void*), const void *data ):
+
+	@asyncio.coroutine
+	def task():
+		while True:
+			cb(data)
+			"""FIXME: this is probably not what we want"""
+			yield from asyncio.sleep(0.1)
+
+	t = asyncio.get_event_loop().create_task(task())
+	return <void*>t
+	
+
+cdef bint wrap_idle_del(void *handle):
+	(<object?>handle).cancel()
+	return True
 
 cdef void quit():
-	pass
+	asyncio.get_event_loop().stop()
 
 cdef void wrap_source_add(args):
 	pass
@@ -50,16 +74,17 @@ cdef void * wrap_source_get_data(args):
 cdef sol_mainloop_implementation _py_asyncio_impl
 
 _py_asyncio_impl.run = wrap_sol_run
-"""_py_asyncio_impl.idle_add = wrap_idle_add
+_py_asyncio_impl.idle_add = wrap_idle_add
 _py_asyncio_impl.idle_del = wrap_idle_del
 _py_asyncio_impl.init = wrap_sol_init
 _py_asyncio_impl.quit = wrap_sol_quit
-_py_asyncio_impl.run = wrap_sol_run
 _py_asyncio_impl.shutdown = wrap_sol_shutdown
+_py_asyncio_impl.timeout_add = wrap_sol_timeout_add
+_py_asyncio_impl.timeout_del = wrap_sol_timeout_del
+"""
 _py_asyncio_impl.source_add = wrap_source_add
 _py_asyncio_impl.source_del = wrap_source_del
 _py_asyncio_impl.source_get_data = wrap_source_get_data
-_py_asyncio_impl.timeout_add = wrap_sol_timeout_add
-_py_asyncio_impl.timeout_del = wrap_sol_timeout_del"""
+"""
 
 sol_mainloop_set_implementation(&_py_asyncio_impl) # to be called on module load
