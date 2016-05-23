bb2c8c69b15d0f3cedcc69f8d8ac215e45a4f3ea
Guilherme Iscaro de Godoy <guilherme.iscaro@intel.com>
2016-05-23 13:45:53 -0300

    AIO node: Remove some unnecessary NULL checks.
    The mdata will never be NULL at that point.
    
    Signed-off-by: Guilherme Iscaro <guilherme.iscaro@intel.com>

--- src/modules/flow/aio/aio.c
+++ src/modules/flow/aio/aio.c
@@ -66,8 +66,6 @@ read_cb(void *cb_data, struct sol_aio *aio, int32_t ret)
     struct sol_irange i;
     struct aio_data *mdata = cb_data;
 
-    SOL_NULL_CHECK(mdata);
-
     i.val = ret;
     if (i.val < 0) {
         sol_flow_send_error_packet(mdata->node, EINVAL,
@@ -93,8 +91,6 @@ static bool
 _on_reader_timeout(void *data)
 {
     struct aio_data *mdata = data;
-
-    SOL_NULL_CHECK(data, true);
 
     mdata->pending = sol_aio_get_value(mdata->aio, read_cb, mdata);
     if (!mdata->pending && errno != -EBUSY) {
