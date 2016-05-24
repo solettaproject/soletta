d6b3e3f003db3ffd78d75a6c7d35427e80feabcb
Wu Zheng <wu.zheng@intel.com>
2016-05-24 15:14:59 +0800

    sol-netctl: Get connection manager state is implemented for connman
    Get connection manager state is implemented.
    It based on connman and wraps the D-Bus APIs of connman.
    Get connection manager state is from connman.
    It includes online, ready, offline, idle.
    
    Signed-off-by: Wu Zheng <wu.zheng@intel.com>

--- src/lib/comms/sol-netctl-impl-connman.c
+++ src/lib/comms/sol-netctl-impl-connman.c
@@ -46,7 +46,9 @@ struct sol_netctl_service {
 
 struct ctx {
     struct sol_bus_client *connman;
+    sd_bus_slot *manager_slot;
     sd_bus_slot *state_slot;
+    enum sol_netctl_state connman_state;
     int32_t refcount;
 };
 
@@ -114,10 +116,101 @@ sol_netctl_service_get_strength(const struct sol_netctl_service *service)
     return service->strength;
 }
 
+static int
+get_manager_properties(sd_bus_message *m)
+{
+    char *state;
+    int r;
+    static const struct sol_str_table table[] = {
+        SOL_STR_TABLE_ITEM("online",
+            SOL_NETCTL_STATE_ONLINE),
+        SOL_STR_TABLE_ITEM("ready",
+            SOL_NETCTL_STATE_READY),
+        SOL_STR_TABLE_ITEM("idle",
+            SOL_NETCTL_STATE_IDLE),
+        SOL_STR_TABLE_ITEM("offline",
+            SOL_NETCTL_STATE_OFFLINE),
+        { }
+    };
+
+    r = sd_bus_message_enter_container(m, SD_BUS_TYPE_VARIANT, "s");
+    SOL_INT_CHECK(r, < 0, r);
+
+    r = sd_bus_message_read_basic(m, SD_BUS_TYPE_STRING, &state);
+    SOL_INT_CHECK(r, < 0, r);
+
+    if (state)
+        _ctx.connman_state = sol_str_table_lookup_fallback(table,
+            sol_str_slice_from_str(state),
+            SOL_NETCTL_STATE_UNKNOWN);
+
+    r = sd_bus_message_exit_container(m);
+    SOL_INT_CHECK(r, < 0, r);
+
+    return 0;
+}
+
+static int
+_manager_properties_changed(sd_bus_message *m, void *userdata,
+    sd_bus_error *ret_error)
+{
+    struct ctx *pending = userdata;
+    char *str;
+    int r;
+
+    pending->manager_slot = sd_bus_slot_unref(pending->manager_slot);
+
+    if (sol_bus_log_callback(m, userdata, ret_error) < 0)
+        return -EINVAL;
+
+    r = sd_bus_message_enter_container(m, SD_BUS_TYPE_ARRAY, "{sv}");
+    SOL_INT_CHECK(r, < 0, r);
+
+    do {
+        r = sd_bus_message_enter_container(m, SD_BUS_TYPE_DICT_ENTRY, "sv");
+        SOL_INT_CHECK_GOTO(r, < 1, end);
+
+        r = sd_bus_message_read_basic(m, SD_BUS_TYPE_STRING, &str);
+        SOL_INT_CHECK(r, < 0, r);
+
+        if (streq(str, "State")) {
+            r = get_manager_properties(m);
+            SOL_INT_CHECK(r, < 0, r);
+        } else {
+            SOL_DBG("Ignored global manager property: %s", str);
+            r = sd_bus_message_skip(m, "v");
+            SOL_INT_CHECK(r, < 0, r);
+        }
+
+        r = sd_bus_message_exit_container(m);
+        SOL_INT_CHECK(r, < 0, r);
+    } while (1);
+
+end:
+    if (r == 0)
+        r = sd_bus_message_exit_container(m);
+    else
+        return r;
+
+    return r;
+}
+
+static int
+dbus_connection_get_manager_properties(void)
+{
+    sd_bus *bus = sol_bus_client_get_bus(_ctx.connman);
+
+    SOL_NULL_CHECK(bus, -EINVAL);
+
+    return sd_bus_call_method_async(bus, &_ctx.manager_slot,
+        "net.connman", "/", "net.connman.Manager", "GetProperties",
+        _manager_properties_changed, &_ctx, NULL);
+}
+
 SOL_API enum sol_netctl_state
 sol_netctl_get_state(void)
 {
-    return SOL_NETCTL_STATE_UNKNOWN;
+    return _ctx.connman_state;
 }
 
 static int
@@ -153,6 +246,9 @@ sol_netctl_set_radios_offline(bool enabled)
 SOL_API bool
 sol_netctl_get_radios_offline(void)
 {
+    if (_ctx.connman_state != SOL_CONNMAN_STATE_OFFLINE)
+        return false;
+
     return true;
 }
 
@@ -186,6 +282,10 @@ sol_netctl_shutdown(void)
 
     _ctx.state_slot =
         sd_bus_slot_unref(_ctx.state_slot);
+    _ctx.manager_slot =
+        sd_bus_slot_unref(_ctx.manager_slot);
+
+    _ctx.connman_state = SOL_NETCTL_STATE_UNKNOWN;
 }
 
 static int
@@ -229,6 +329,10 @@ sol_netctl_shutdown_lazy(void)
 
     _ctx.state_slot =
         sd_bus_slot_unref(_ctx.state_slot);
+    _ctx.manager_slot =
+        sd_bus_slot_unref(_ctx.manager_slot);
+
+    _ctx.connman_state = SOL_NETCTL_STATE_UNKNOWN;
 }
 
 SOL_API int
