solettaproject/soletta

commit 96d90e52d77ad0a63137f0d9036e906fa23eff37

network: Fix API
Change API names
Change the return of some functions for -errno

issue #1716

Signed-off-by: Amanda Coelho <amada.winder.ribeiro.coelho@intel.com>

src/samples/network/network-status.c
@@ -107,7 +107,7 @@ _on_network_event(void *data, const struct sol_network_link *link, enum sol_netw
         struct sol_network_link_addr *addr;
         uint16_t i;
 
-        SOL_BUFFER_DECLARE_STATIC(addr_str, SOL_INET_ADDR_STRLEN);
+        SOL_BUFFER_DECLARE_STATIC(addr_str, SOL_NETWORK_INET_ADDR_STR_LEN);
 
         printf("\tUP ");
         SOL_VECTOR_FOREACH_IDX (&link->addrs, addr, i) {

data/scripts/sol-oic-gen.py
@@ -1168,8 +1168,8 @@ state_changed(void *data, enum sol_coap_response_code response_code, struct sol_
         return;
 
     if (!sol_network_link_addr_eq(cliaddr, &resource->resource->addr)) {
-        SOL_BUFFER_DECLARE_STATIC(resaddr, SOL_INET_ADDR_STRLEN);
-        SOL_BUFFER_DECLARE_STATIC(respaddr, SOL_INET_ADDR_STRLEN);
+        SOL_BUFFER_DECLARE_STATIC(resaddr, SOL_NETWORK_INET_ADDR_STR_LEN);
+        SOL_BUFFER_DECLARE_STATIC(respaddr, SOL_NETWORK_INET_ADDR_STR_LEN);
 
         if (!sol_network_link_addr_to_str(&resource->resource->addr, &resaddr)) {
             SOL_WRN("Could not convert network address to string");

src/bin/sol-fbp-runner/web-inspector.c
@@ -1436,7 +1436,7 @@ url_from_addr(struct sol_buffer *url_buf, const struct sol_network_link_addr *ad
     };
     int r;
 
-    SOL_BUFFER_DECLARE_STATIC(addr_buf, SOL_INET_ADDR_STRLEN);
+    SOL_BUFFER_DECLARE_STATIC(addr_buf, SOL_NETWORK_INET_ADDR_STR_LEN);
 
     if (!sol_network_link_addr_to_str(addr, &addr_buf))
         return -EINVAL;

src/lib/comms/include/sol-network.h
@@ -58,7 +58,7 @@ extern "C" {
 /**
  * @brief String size of an IPv4/v6 address.
  */
-#define SOL_INET_ADDR_STRLEN 48
+#define SOL_NETWORK_INET_ADDR_STR_LEN 48
 
 /**
  * @brief String size of a Bluetooth address.
@@ -66,17 +66,17 @@ extern "C" {
 #define SOL_BLUETOOTH_ADDR_STRLEN 18
 
 /**
- * @struct sol_network_hostname_handle
+ * @struct sol_network_hostname_pending
  *
  * @brief A handle to sol_network_get_hostname_address_info()
  *
  * This handle can be used to cancel get sol_network_get_hostname_address_info()
- * by calling sol_network_cancel_get_hostname_address_info()
+ * by calling sol_network_hostname_pending_cancel()
  *
  * @see sol_network_get_hostname_address_info()
- * @see sol_network_cancel_get_hostname_address_info()
+ * @see sol_network_hostname_pending_cancel()
  */
-struct sol_network_hostname_handle;
+struct sol_network_hostname_pending;
 
 /**
  * @brief Type of events generated for a network link.
@@ -268,11 +268,11 @@ sol_network_link_addr_eq(const struct sol_network_link_addr *a,
  * @param cb The callback used to notify the user.
  * @param data The user data given in the callback.
  *
- * @return @c true on success, @c false on error
+ * @return @c true on success, @c -errno on error
  *
  * @see sol_network_unsubscribe_events()
  */
-bool sol_network_subscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
+int sol_network_subscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
     enum sol_network_event event),
     const void *data);
 
@@ -285,12 +285,12 @@ bool sol_network_subscribe_events(void (*cb)(void *data, const struct sol_networ
  * @param cb The callback given on @ref sol_network_subscribe_events.
  * @param data The data given on @ref sol_network_subscribe_events.
  *
- * @return @c true on success, @c false on error
+ * @return @c O on success, @c -errno on error
  *
  * @note It should be the same pair (callback/userdata) given on @ref
  * sol_network_subscribe_events()
  */
-bool sol_network_unsubscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
+int sol_network_unsubscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
     enum sol_network_event event),
     const void *data);
 /**
@@ -321,11 +321,11 @@ char *sol_network_link_get_name(const struct sol_network_link *link);
  * able to get a network address.
  *
  * @param link_index The index of a @ref sol_network_link structure.
- * @return @c true on success, @c false on error.
+ * @return @c true on success, @c -errno on error.
  *
  * @see sol_network_linke_down()
  */
-bool sol_network_link_up(uint16_t link_index);
+int sol_network_link_up(uint16_t link_index);
 
 /**
  * @brief Sets a network link down.
@@ -334,11 +334,11 @@ bool sol_network_link_up(uint16_t link_index);
  * able to get a network address.
  *
  * @param link_index The index of a @ref sol_network_link structure.
- * @return @c true on success, @c false on error.
+ * @return @c 0 on success, @c -ENOSYS on error.
  *
  * @see sol_network_linke_up()
  */
-bool sol_network_link_down(uint16_t link_index);
+int sol_network_link_down(uint16_t link_index);
 
 /**
  * @brief Gets a hostname address info.
@@ -356,10 +356,10 @@ bool sol_network_link_down(uint16_t link_index);
  * @param host_info_cb A callback to be called with the address list.
  * @param data Data to @c host_info_cb.
  * @return A handle to a hostname or @c NULL on error.
- * @see sol_network_cancel_get_hostname_address_info()
+ * @see sol_network_hostname_pending_cancel()
  * @see #sol_network_family
  */
-struct sol_network_hostname_handle *
+struct sol_network_hostname_pending *
 sol_network_get_hostname_address_info(const struct sol_str_slice hostname,
     enum sol_network_family family, void (*host_info_cb)(void *data,
     const struct sol_str_slice hostname, const struct sol_vector *addrs_list),
@@ -372,7 +372,7 @@ sol_network_get_hostname_address_info(const struct sol_str_slice hostname,
  * @return 0 on success, -errno on error.
  * @see sol_network_get_hostname_address_info()
  */
-int sol_network_cancel_get_hostname_address_info(struct sol_network_hostname_handle *handle);
+int sol_network_hostname_pending_cancel(struct sol_network_hostname_pending *handle);
 
 /**
  * @}

src/lib/comms/sol-coap.c
@@ -621,7 +621,7 @@ timeout_cb(void *data)
     struct outgoing *outgoing = data;
     struct sol_coap_server *server = outgoing->server;
 
-    SOL_BUFFER_DECLARE_STATIC(addr, SOL_INET_ADDR_STRLEN);
+    SOL_BUFFER_DECLARE_STATIC(addr, SOL_NETWORK_INET_ADDR_STR_LEN);
 
     outgoing->timeout = NULL;
 
@@ -793,7 +793,7 @@ on_can_write(void *data, struct sol_socket *s)
     sol_coap_packet_debug(outgoing->pkt);
     if (ret < 0) {
         uint16_t id;
-        SOL_BUFFER_DECLARE_STATIC(addr, SOL_INET_ADDR_STRLEN);
+        SOL_BUFFER_DECLARE_STATIC(addr, SOL_NETWORK_INET_ADDR_STR_LEN);
 
         sol_network_link_addr_to_str(&outgoing->cliaddr, &addr);
         sol_coap_header_get_id(outgoing->pkt, &id);
@@ -919,7 +919,7 @@ sol_coap_send_packet_with_reply(struct sol_coap_server *server, struct sol_coap_
 done:
     err = enqueue_packet(server, pkt, NULL, cliaddr);
     if (err < 0) {
-        SOL_BUFFER_DECLARE_STATIC(addr, SOL_INET_ADDR_STRLEN);
+        SOL_BUFFER_DECLARE_STATIC(addr, SOL_NETWORK_INET_ADDR_STR_LEN);
 
         sol_network_link_addr_to_str(cliaddr, &addr);
         SOL_WRN("Could not enqueue packet %p to %.*s (%d): %s", pkt,
@@ -1004,7 +1004,7 @@ sol_coap_notify(struct sol_coap_server *server,
 
         r = enqueue_packet(server, pkt, header, &o->cliaddr);
         if (r < 0) {
-            SOL_BUFFER_DECLARE_STATIC(addr, SOL_INET_ADDR_STRLEN);
+            SOL_BUFFER_DECLARE_STATIC(addr, SOL_NETWORK_INET_ADDR_STR_LEN);
 
             sol_network_link_addr_to_str(&o->cliaddr, &addr);
             SOL_WRN("Failed to enqueue packet %p to %.*s", header,

src/lib/comms/sol-lwm2m.c
@@ -239,7 +239,7 @@ struct sol_lwm2m_client {
 };
 
 struct server_conn_ctx {
-    struct sol_network_hostname_handle *hostname_handle;
+    struct sol_network_hostname_pending *hostname_handle;
     struct sol_lwm2m_client *client;
     struct sol_vector server_addr_list;
     struct sol_coap_packet *pending_pkt; //Pending registration reply
@@ -3215,7 +3215,7 @@ server_connection_ctx_clear(struct server_conn_ctx *conn_ctx)
     if (conn_ctx->pending_pkt)
         sol_coap_packet_unref(conn_ctx->pending_pkt);
     if (conn_ctx->hostname_handle)
-        sol_network_cancel_get_hostname_address_info(conn_ctx->hostname_handle);
+        sol_network_hostname_pending_cancel(conn_ctx->hostname_handle);
     sol_vector_clear(&conn_ctx->server_addr_list);
     free(conn_ctx->location);
 }
@@ -3486,7 +3486,7 @@ register_reply(void *data, struct sol_coap_server *server,
     uint8_t code;
     int r;
 
-    SOL_BUFFER_DECLARE_STATIC(addr, SOL_INET_ADDR_STRLEN);
+    SOL_BUFFER_DECLARE_STATIC(addr, SOL_NETWORK_INET_ADDR_STR_LEN);
 
     sol_coap_packet_unref(conn_ctx->pending_pkt);
     conn_ctx->pending_pkt = NULL;

src/lib/comms/sol-network-impl-contiki.c
@@ -46,7 +46,7 @@ sol_network_link_addr_to_str(const struct sol_network_link_addr *addr,
     if (addr->family != SOL_NETWORK_FAMILY_INET6)
         return NULL;
 
-    if (buf->capacity - buf->used < SOL_INET_ADDR_STRLEN)
+    if (buf->capacity - buf->used < SOL_NETWORK_INET_ADDR_STR_LEN)
         return NULL;
 
     p = sol_buffer_at_end(buf);
@@ -161,20 +161,20 @@ sol_network_shutdown(void)
     return;
 }
 
-SOL_API bool
+SOL_API int
 sol_network_subscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
     enum sol_network_event event),
     const void *data)
 {
-    return false;
+    return -ENOSYS;
 }
 
-SOL_API bool
+SOL_API int
 sol_network_unsubscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
     enum sol_network_event event),
     const void *data)
 {
-    return false;
+    return -ENOSYS;
 }
 
 SOL_API const struct sol_vector *

src/lib/comms/sol-network-impl-linux.c
@@ -51,7 +51,7 @@ struct callback {
     const void *data;
 };
 
-struct sol_network_hostname_handle {
+struct sol_network_hostname_pending {
     char *hostname;
     enum sol_network_family family;
     void (*cb)(void *data, const struct sol_str_slice hostname,
@@ -96,7 +96,7 @@ sol_network_link_addr_to_str(const struct sol_network_link_addr *addr,
         if (r || (!r && errno != ENOSPC))
             break;
 
-        err = sol_buffer_expand(buf, SOL_INET_ADDR_STRLEN);
+        err = sol_buffer_expand(buf, SOL_NETWORK_INET_ADDR_STR_LEN);
         SOL_INT_CHECK(err, < 0, NULL);
     }
 
@@ -413,7 +413,7 @@ sol_network_init(void)
 }
 
 static void
-hostname_handle_free(struct sol_network_hostname_handle *ctx)
+hostname_handle_free(struct sol_network_hostname_pending *ctx)
 {
     free(ctx->hostname);
     free(ctx);
@@ -424,7 +424,7 @@ sol_network_shutdown(void)
 {
     struct sol_network_link *link;
     uint16_t idx;
-    struct sol_network_hostname_handle *ctx;
+    struct sol_network_hostname_pending *ctx;
 
     if (!network)
         return;
@@ -455,42 +455,45 @@ sol_network_shutdown(void)
     network = NULL;
 }
 
-SOL_API bool
+SOL_API int
 sol_network_subscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
     enum sol_network_event event),
     const void *data)
 {
+    int r;
+
     struct callback *callback;
 
-    SOL_NULL_CHECK(cb, false);
+    SOL_NULL_CHECK(cb, -EINVAL);
 
-    if (sol_network_start_netlink() < 0) return false;
+    r = sol_network_start_netlink();
+    SOL_INT_CHECK(r, < 0, r);
 
     callback = sol_vector_append(&network->callbacks);
-    SOL_NULL_CHECK(callback, false);
+    SOL_NULL_CHECK(callback, -ENOMEM);
 
     callback->cb = cb;
     callback->data = data;
 
-    return true;
+    return 0;
 }
 
-SOL_API bool
+SOL_API int
 sol_network_unsubscribe_events(void (*cb)(void *data, const struct sol_network_link *link,
     enum sol_network_event event),
     const void *data)
 {
     struct callback *callback;
-    bool ret = false;
+    int ret = -EFAULT;
     uint16_t idx;
 
-    SOL_NULL_CHECK(network, false);
-    SOL_NULL_CHECK(cb, false);
+    SOL_NULL_CHECK(network, -EINVAL);
+    SOL_NULL_CHECK(cb, -EINVAL);
 
     SOL_VECTOR_FOREACH_REVERSE_IDX (&network->callbacks, callback, idx) {
         if ((callback->cb == cb) && (callback->data == data)) {
             sol_vector_del(&network->callbacks, idx);
-            ret = true;
+            ret = 0;
         }
     }
 
@@ -519,9 +522,10 @@ sol_network_link_get_name(const struct sol_network_link *link)
     return NULL;
 }
 
-static bool
+static int
 sol_network_link_set_status(uint16_t link_index, unsigned int changes, unsigned int flags)
 {
+    int r;
     char buf[NLMSG_ALIGN(sizeof(struct nlmsghdr) + sizeof(struct ifinfomsg) + 512)] = { 0 };
     struct iovec iov = { buf, sizeof(buf) };
     struct msghdr msg = { 0 };
@@ -532,7 +536,8 @@ sol_network_link_set_status(uint16_t link_index, unsigned int changes, unsigned 
     uint8_t mode = IN6_ADDR_GEN_MODE_EUI64;
     size_t buf_size = sizeof(buf);;
 
-    if (sol_network_start_netlink() < 0) return false;
+    r = sol_network_start_netlink();
+    SOL_INT_CHECK(r, < 0, r);
 
     msg.msg_name = (void *)&snl;
     msg.msg_namelen = sizeof(snl);
@@ -574,19 +579,19 @@ sol_network_link_set_status(uint16_t link_index, unsigned int changes, unsigned 
 
     if (sendmsg(network->nl_socket, (struct msghdr *)&msg, 0) <= 0) {
         SOL_WRN("Failed on send message to set link up");
-        return false;
+        return -errno;
     }
 
-    return true;
+    return 0;
 }
 
-SOL_API bool
+SOL_API int
 sol_network_link_up(uint16_t link_index)
 {
     return sol_network_link_set_status(link_index, IFF_UP, IFF_UP);
 }
 
-SOL_API bool
+SOL_API int
 sol_network_link_down(uint16_t link_index)
 {
     return sol_network_link_set_status(link_index, IFF_UP, ~IFF_UP);
@@ -596,7 +601,7 @@ static bool
 hostname_worker(void *data)
 {
     uint16_t i;
-    struct sol_network_hostname_handle *ctx;
+    struct sol_network_hostname_pending *ctx;
     int r;
     struct addrinfo hints = { };
     uint8_t **base_data = network->hostname_handles.base.data;
@@ -669,19 +674,19 @@ err_getaddr:
     return false;
 }
 
-SOL_API struct sol_network_hostname_handle *
+SOL_API struct sol_network_hostname_pending *
 sol_network_get_hostname_address_info(const struct sol_str_slice hostname,
     enum sol_network_family family, void (*host_info_cb)(void *data,
     const struct sol_str_slice host, const struct sol_vector *addrs_list),
     const void *data)
 {
-    struct sol_network_hostname_handle *ctx;
+    struct sol_network_hostname_pending *ctx;
     int r;
 
     SOL_NULL_CHECK(host_info_cb, NULL);
     SOL_NULL_CHECK(network, NULL);
 
-    ctx = calloc(1, sizeof(struct sol_network_hostname_handle));
+    ctx = calloc(1, sizeof(struct sol_network_hostname_pending));
     SOL_NULL_CHECK(ctx, NULL);
 
     r = sol_ptr_vector_append(&network->hostname_handles, ctx);
@@ -709,8 +714,8 @@ err_append:
 }
 
 SOL_API int
-sol_network_cancel_get_hostname_address_info(
-    struct sol_network_hostname_handle *handle)
+sol_network_hostname_pending_cancel(
+    struct sol_network_hostname_pending *handle)
 {
     int r;
 

commit a7bddc985f3900df5d79b9dd1bb9f133a19307b4

network: fix usage of subscribe regarding return values
After changes on API from returning bool to int, some
usages weren't updated.

So some samples and node types are broken since commit
96d90e52d77ad0a network: Fix API

Signed-off-by: Bruno Dilly <bruno.dilly@intel.com>

src/modules/flow/network/network.c
@@ -190,8 +190,8 @@ network_open(struct sol_flow_node *node, void *data, const struct sol_flow_node_
     SOL_FLOW_NODE_OPTIONS_SUB_API_CHECK(options,
         SOL_FLOW_NODE_TYPE_NETWORK_BOOLEAN_OPTIONS_API_VERSION, -EINVAL);
 
-    if (!sol_network_subscribe_events(_on_network_event, mdata))
-        return -EINVAL;
+    r = sol_network_subscribe_events(_on_network_event, mdata);
+    SOL_INT_CHECK(r, < 0, r);
 
     sol_ptr_vector_init(&mdata->links);
     mdata->node = node;

src/modules/linux-micro/network-up/network-up.c
@@ -62,10 +62,12 @@ network_up_start(const struct sol_platform_linux_micro_module *mod, const char *
 static int
 network_up_init(const struct sol_platform_linux_micro_module *module, const char *service)
 {
+    int r;
+
     SOL_LOG_INTERNAL_INIT_ONCE;
 
-    if (!sol_network_subscribe_events(_network_event_cb, NULL))
-        return -1;
+    r = sol_network_subscribe_events(_network_event_cb, NULL);
+    SOL_INT_CHECK(r, < 0, r);
 
     return 0;
 }

src/samples/network/network-status.c
@@ -127,7 +127,7 @@ static void
 startup_network(void)
 {
     char **argv = sol_argv();
-    int c, opt_idx,  argc = sol_argc();
+    int c, r, opt_idx,  argc = sol_argc();
     char *regexp = NULL;
     static const struct option opts[] = {
         { "interface", required_argument, NULL, 'i' },
@@ -154,8 +154,8 @@ startup_network(void)
     if (!_compile_regex(regexp))
         goto err;
 
-    if (!sol_network_subscribe_events(_on_network_event, NULL))
-        goto err_init;
+    r = sol_network_subscribe_events(_on_network_event, NULL);
+    SOL_INT_CHECK_GOTO(r, < 0, err_init);
 
     return;
 